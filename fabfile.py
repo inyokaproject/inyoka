2712f0d0ffef95f9b377b531206acdb41c24bbfc
Christopher Grebs <cg@webshox.org>
2011-08-13 12:23:26 +0200

    Updated fabfile to new git infrastructure.
    This commit also removes a lot of manage.py aliases so that the fabfile
    is only for deployment now and all special commands should be implemented
    as django management commands.

--- fabfile.py
+++ fabfile.py
@@ -9,47 +9,32 @@
     :license: GNU GPL.
 """
 import os
-from fabric.api import env, run, local, put, require, prompt, open_shell
+from fabric.api import env, run, local, put, require, prompt, open_shell, \
+    roles, settings
 from fabric.contrib.project import rsync_project
 from fabric.context_managers import cd
-from tempfile import mktemp as _mktemp
-
-env.user = 'ubuntu_de'
-INYOKA_REPO = 'https://inyoka:Faraex3d@hg.apolloner.eu/inyoka-prod/'
 
 
-def test():
-    """
-    Fabric target for localhost
-    """
-    env.hosts = ['127.0.0.1']
-    env.user = 'ubuntu_de'
+env.roledefs.update({
+    'web': ['ubuntu_de@dongo.ubuntu-eu.org',
+            'ubuntu_de@unkul.ubuntu-eu.org',
+            'ubuntu_de@oya.ubuntu-eu.org'],
+    'static': ['apollo13@lisa.ubuntu-eu.org']
+})
 
-def static():
-    """
-    Fabric target for static files
-    """
-    env.hosts = ['lisa.ubuntu-eu.org']
-    env.repository = INYOKA_REPO
-    env.target_dir = '/home/ubuntu_de_static'
-    env.user = 'apollo13'
+env.repository = 'git@github.com:inyokaproject/inykoa'
+env.target_dir = '~/virtualenv/inyoka'
 
-def production():
-    """
-    Fabric target for ubuntu-eu.org production servers
-    """
-    env.hosts = ['dongo.ubuntu-eu.org', 'unkul.ubuntu-eu.org', 'oya.ubuntu-eu.org']
-    env.repository = INYOKA_REPO
-    env.target_dir = '~/virtualenv/inyoka'
-    env.user = 'ubuntu_de'
+STATIC_DIRECTORY = '/home/ubuntu_de_static'
+
 
 def bootstrap():
     """Create a virtual environment.  Call this once on every new server."""
-    env.hosts = [x.strip() for x in raw_input('Servers: ').split(',')],
+    env.hosts = [x.strip() for x in raw_input('Servers: ').split(',')]
     env.interpreter = raw_input('Python-executable (default: python2.5): ').strip() or 'python2.5'
-    env.target_dir = raw_input('Location (default: ~/virtualenv): ').strip().rstrip('/') or '~/virtualenv'
+    env.target_dir = raw_input('Location (default: %s): ' % TARGET_DIR).strip().rstrip('/') or TARGET_DIR
     run('mkdir {target_dir}'.format(target_dir=env.target_dir))
-    run('hg clone {repository} {target_dir}/inyoka'.format(target_dir=env.target_dir))
+    run('git clone {repository} {target_dir}/inyoka'.format(target_dir=env.target_dir))
     run('{interpreter} {target_dir}/inyoka/make-bootstrap.py > {target_dir}/bootstrap.py'.format(**{
         'interpreter': env.interpreter,
         'target_dir': env.target_dir}))
@@ -60,24 +45,25 @@ def bootstrap():
         'target_dir': env.target_dir,
         'interpreter': env.interpreter}))
 
+
+@roles('web')
 def deploy():
     """Update Inyoka and touch the wsgi file"""
-    require('hosts', provided_by=[test, production])
     run('unset PYTHONPATH;'
         'source {target_dir}/../bin/activate;'
-        'hg pull -R {target_dir} -u {repository}'.format(**{
-            'target_dir': env.target_dir,
-            'repository': env.repository}))
+        'git fetch origin master')
 
+
+@roles('static')
 def deploy_static():
     """Deploy static files"""
-    require('hosts', provided_by=[static])
-    local('fab compile_static', capture=False)
-    rsync_project(env.target_dir, 'inyoka/static')
+    compile_static()
+    with settings(target_dir=STATIC_DIRECTORY):
+        rsync_project(env.target_dir, 'inyoka/static')
+
 
 def pip():
     """Run pip on the server"""
-    require('hosts', provided_by=[test, production])
     if not getattr(env, 'parameters', None):
         prompt('pip parameters', key='parameters')
     run('unlet PYTHONPATH;'
@@ -85,73 +71,6 @@ def pip():
         'pip {parameters}'.format(**{
             'target_dir': env.target_dir,
             'parameters': env.parameters}))
-
-
-def run_tests(module=''):
-    """Run tests"""
-    local('python manage.py test --failfast {module}'.format(**{
-        'module': module
-    }), capture=False)
-
-
-def reindent():
-    """Reindent all python sources"""
-    local("extra/reindent.py -r -B .", capture=False)
-
-
-def syncdb():
-    """Sync database models"""
-    local("python manage.py syncdb", capture=False)
-
-
-def migrate():
-    """Migrate database"""
-    local("python manage.py migrate", capture=False)
-
-
-def create_test_data():
-    """Creates some data, usefull for testing inyoka without production db dump"""
-    local("python make_testdata.py", capture=False)
-
-
-def server():
-    """Start development server"""
-    local("python manage.py runserver", capture=False)
-
-
-def shell():
-    """Start development shell"""
-    local("python manage.py shell", capture=False)
-
-
-def bash():
-    """start a shell within the current context"""
-    open_shell()
-
-
-def dbshell():
-    """
-    Start a Database Shell for the configured database
-    """
-    local("python manage.py dbshell", capture=False)
-
-
-def celeryd():
-    """
-    Start a celery worker, using our config.
-    """
-    # we start celery with -B option to make periodic tasks possible
-    cmd = ('python manage.py celeryd --loglevel=DEBUG -B')
-    local(cmd, capture=False)
-
-
-def clean_files():
-    """Clean most temporary files"""
-    for f in '*.py[co]', '*~', '*.orig', '*.rej':
-        local("rm -rf `find -name '%s'|grep -v .hg`" % f)
-
-    for f in '*.css', '*.min.js':
-        local("rm -rf `find inyoka/static -name '%s'|grep -v .hg|grep -v mobile.css`" % f)
 
 
 def check_js():
