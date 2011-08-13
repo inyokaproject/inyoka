bee6558ef2971af6637ae4371b006fca16c7601f
Sören Wegener <wegener92@googlemail.com>
2011-08-13 22:16:35 +0200

    Replaced some more quotes.

--- inyoka/portal/views.py
+++ inyoka/portal/views.py
@@ -224,9 +224,9 @@ def register(request):
                 user.settings['timezone'] = timezone
                 user.save()
 
-            flash(_('The username "%(username)s" was successfully registered. '
+            flash(_('The username “%(username)s“ was successfully registered. '
                     'An email with an activationkey was sent to '
-                    '"%(email)s".') % {
+                    '“%(email)s“.') % {
                         'username': escape(user.username),
                         'email': escape(user.email)
                     }, True)
@@ -248,7 +248,7 @@ def activate(request, action='', username='', activation_key=''):
     try:
         user = User.objects.get(username)
     except User.DoesNotExist:
-        flash(_('The user "%(username)s” does not exist.') % {
+        flash(_('The user “%(username)s“ does not exist.') % {
             'username': escape(username)}, False)
         return HttpResponseRedirect(href('portal'))
     if not redirect:
@@ -271,7 +271,7 @@ def activate(request, action='', username='', activation_key=''):
                 #      escape(username), True)
                 flash(_('Your account was anonymized.'), True)
             else:
-                flash(_('The account "%(username)s" was already activated.') %
+                flash(_('The account “%(username)s“ was already activated.') %
                       {'username': escape(username)}, False)
         else:
             flash(_('Your activationkey is invalid.'), False)
@@ -294,7 +294,7 @@ def resend_activation_mail(request, username):
     user = User.objects.get(username)
 
     if user.status > 0:
-        flash(_('The account "%(username)s" was already activated.') %
+        flash(_('The account “%(username)s“ was already activated.') %
               {'username': escape(user.username)}, False)
         return HttpResponseRedirect(href('portal'))
     send_activation_mail(user)
@@ -587,7 +587,7 @@ def user_mail(request, username):
             #    flash(u'Die Mail konnte nicht verschickt werden.')
             #    return HttpResponseRedirect(href('admin', 'users', 'mail',
             #                                 escape(username)))
-            flash(_('The email to "%(username)s" was sent successfully.')
+            flash(_('The email to “%(username)s“ was sent successfully.')
                   % {'username': escape(username)}, True)
             return HttpResponseRedirect(request.GET.get('next') or href('portal', 'users'))
         else:
@@ -608,7 +608,7 @@ def subscribe_user(request, username):
     except Subscription.DoesNotExist:
         # there's no such subscription yet, create a new one
         Subscription(user=request.user, content_object=user).save()
-        flash(_('You will now be notified about activities of "%(username)s”.')
+        flash(_('You will now be notified about activities of “%(username)s“.')
               % {'username': user.username})
     return HttpResponseRedirect(url_for(user))
 
@@ -623,7 +623,7 @@ def unsubscribe_user(request, username):
     else:
         subscription.delete()
         flash(_('You will now not be notfied anymore about activities of '
-                '"%(username)s".') % {'username': user.username})
+                '“%(username)s“.') % {'username': user.username})
     return HttpResponseRedirect(url_for(user))
 
 
@@ -986,7 +986,7 @@ def user_edit_profile(request, username):
                             })
 
             user.save()
-            flash(_('The profile of "%(username)s" was changed successfully')
+            flash(_('The profile of “%(username)s“ was changed successfully')
                     % {'username': escape(user.username)}, True)
             # redirect to the new username if given
             if user.username != username:
@@ -1048,7 +1048,7 @@ def user_edit_settings(request, username):
             for key, value in data.iteritems():
                 user.settings[key] = data[key]
             user.save()
-            flash(_('The setting of "%(username)s" were successfully changed.')
+            flash(_('The setting of “%(username)s“ were successfully changed.')
                   % {'username': escape(user.username)}, True)
     return {
         'user': user,
@@ -1072,7 +1072,7 @@ def user_edit_status(request, username):
             for key in ('status', 'banned_until',):
                 setattr(user, key, data[key])
             user.save()
-            flash(_('The status of "%(username)s” was successfully changed.')
+            flash(_('The status of “%(username)s“ was successfully changed.')
                   % {'username': escape(user.username)}, True)
     if user.status > 0:
         activation_link = None
@@ -1096,7 +1096,7 @@ def user_edit_password(request, username):
         data = form.cleaned_data
         user.set_password(data['new_password'])
         user.save()
-        flash(_('The password of "%(username)s" was successfully changed.')
+        flash(_('The password of “%(username)s“ was successfully changed.')
               % {'username': escape(user.username)}, True)
     return {
         'user': user,
@@ -1156,7 +1156,7 @@ def user_edit_privileges(request, username):
             user.save()
             cache.delete('user_permissions/%s' % user.id)
 
-            flash(_('The privileges of "%(username)s" were successfully '
+            flash(_('The privileges of “%(username)s“ were successfully '
                     'changed.') % {'username': escape(user.username)}, True)
         else:
             flash(_('Errors occurred, please fix them.'), False)
@@ -1244,7 +1244,7 @@ def user_edit_groups(request, username):
             user._primary_group = primary
 
             user.save()
-            flash(_('The groups of "%(username)s" were successfully changed.')
+            flash(_('The groups of “%(username)s“ were successfully changed.')
                   % {'username': escape(user.username)}, True)
         else:
             flash(_('Errors occurred, please fix them.'), False)
@@ -1272,7 +1272,7 @@ def user_new(request):
                 email=data['email'],
                 password=data['password'],
                 send_mail=data['authenticate'])
-            flash(_('The user "%(username)s" was successfully created. '
+            flash(_('The user “%(username)s“ was successfully created. '
                     'You can now edit more details.')
                   % {'username': escape(data['username'])}, True)
             return HttpResponseRedirect(href('portal', 'user', \
@@ -1290,7 +1290,7 @@ def user_new(request):
 def admin_resend_activation_mail(request):
     user = User.objects.get(request.GET.get('user'))
     if user.status != 0:
-        flash(_('The account "%(username)s" was already activated.')
+        flash(_('The account “%(username)s“ was already activated.')
               % {'username': user.username})
     else:
         send_activation_mail(user)
@@ -1457,7 +1457,7 @@ def privmsg_new(request, username=None):
                         all().exclude(pk=request.user.id)
                     recipients.update(users)
                 except Group.DoesNotExist:
-                    flash(_('The group "%(group)s" was not found.')
+                    flash(_('The group “%(group)s“ was not found.')
                           % {'group': escape(group)}, False)
                     return HttpResponseRedirect(href('portal', 'privmsg'))
 
