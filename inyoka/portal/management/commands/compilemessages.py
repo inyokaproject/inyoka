8d2bd32aab2977c127819235d4aacddc2f7bbc8a
Sören Wegener <wegener92@googlemail.com>
2011-08-13 14:49:34 +0200

    Custom manage.py commands
    They replace the default "makemessages" and "compilemessages" commands.
    Use compilemessages in fabfiles compile_translations.

---
+++ inyoka/portal/management/commands/compilemessages.py
@@ -0,0 +1,12 @@
+from subprocess import call
+
+from django.core.management.base import BaseCommand
+
+APPS = ['forum', 'portal', 'wiki', 'ikhaya', 'utils', 'pastebin', 'planet']
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        for app in APPS:
+            call(['pybabel', 'compile', '-D', 'django', '-d',
+                  'inyoka/%s/locale' % app, '-l', 'de'])
