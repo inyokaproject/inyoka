329b73fe186666bbb760b47deef5ff910f52525b
Florian Apolloner <florian@apolloner.eu>
2011-08-16 18:42:31 +0200

    added search.autodiscover to discover search modules

--- inyoka/utils/search.py
+++ inyoka/utils/search.py
@@ -187,5 +187,29 @@ class SearchSystem(object):
         return self.get_connection().search(query=search)
 
 
+def autodiscover():
+    """
+    Auto-discover INSTALLED_APPS search.py modules and fail silently when
+    not present. This forces an import on them to register any search bits they
+    may want.
+    """
+
+    from django.conf import settings
+    from django.utils.importlib import import_module
+    from django.utils.module_loading import module_has_submodule
+
+    for app in settings.INSTALLED_APPS:
+        mod = import_module(app)
+        # Attempt to import the app's search module.
+        try:
+            import_module('%s.search' % app)
+        except:
+            # Decide whether to bubble up this error. If the app just
+            # doesn't have a search module, we can ignore the error
+            # attempting to import it, otherwise we want it to bubble up.
+            if module_has_submodule(mod, 'search'):
+                raise
+
+
 #: Singleton that implements the search system
 search = SearchSystem()
