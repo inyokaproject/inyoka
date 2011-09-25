fddde1a0eedb4553cc658504c42bbc7e491c736d
Christopher Grebs <cg@webshox.org>
2011-09-25 15:21:23 +0200

    More cleanups

--- inyoka/hosts.py
+++ inyoka/hosts.py
@@ -1,3 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+    inyoka.hosts
+    ~~~~~~~~~~~~
+
+    Subdomain specifications.
+
+    :copyright: (c) 2007-2011 by the Inyoka Team, see AUTHORS for more details.
+    :license: GNU GPL, see LICENSE for more details.
+"""
 import sys
 
 from django.conf import settings
@@ -8,8 +18,7 @@ from inyoka.utils.http import HttpResponsePermanentRedirect
 
 def redirect_www(request):
     """Redirects www. to settings.BASE_DOMAIN_NAME"""
-    host = request.get_host()
-    host = host[4:]
+    host = request.get_host()[4:]
     return HttpResponsePermanentRedirect('//%s%s' % (host, request.path))
 
 
