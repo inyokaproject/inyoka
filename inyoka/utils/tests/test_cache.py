f880b3d575f82ecd750ced03b997a5fbd8e040f3
Christopher Grebs <cg@webshox.org>
2011-09-30 13:53:57 +0200

    Update utils.cache tests

--- inyoka/utils/tests/test_cache.py
+++ inyoka/utils/tests/test_cache.py
@@ -24,15 +24,11 @@ class TestCache(TestCase):
         self.request_cache = get_cache('request')
         self.request_cache.request_cache = self.real
 
-    def test_set(self):
+    def test_seperate(self):
         def _compare(key, value, exists=True):
             self.assertEqual(value, self.cache.get(key))
             self.assertEqual(value, self.request_cache.get(key))
             self.assertEqual(key in self.request_cache.request_cache, exists)
-
-        def _compare_many(keys, value):
-            self.assertEqual(value, self.cache.get_many(keys))
-            self.assertEqual(value, self.request_cache.get_many(keys))
 
         self.request_cache.set('test', 'foo')
         self.request_cache.set('test', 'bar')
@@ -40,7 +36,30 @@ class TestCache(TestCase):
         _compare('test', 'bar')
         _compare('blah', None, False)
         _compare('bar', 'foo')
+
+
+    def test_many(self):
+        def _compare_many(keys, value):
+            self.assertEqual(value, self.cache.get_many(keys))
+            self.assertEqual(value, self.request_cache.get_many(keys))
+
+        def _compare(key, value, exists=True):
+            self.assertEqual(value, self.cache.get(key))
+            self.assertEqual(value, self.request_cache.get(key))
+            self.assertEqual(key in self.request_cache.request_cache, exists)
+
+        self.request_cache.set_many({
+            'test': 'bar',
+            'bar': 'foo'
+        })
+
         _compare_many(('test', 'bar', 'blah'), {'test': 'bar', 'bar': 'foo'})
         _compare('test', 'bar')
         _compare('bar', 'foo')
         _compare('blah', None, False)
+
+    def test_delete(self):
+        self.request_cache.set('foo', 'bar')
+        self.assertEqual(self.request_cache.get('foo'), 'bar')
+        self.request_cache.delete('foo')
+        self.assertEqual(self.request_cache.get('foo'), None)
