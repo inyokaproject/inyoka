09d8643809b3bc1efb14591cbfe12559d141c510
Christopher Grebs <cg@webshox.org>
2011-09-30 13:15:35 +0200

    Updated request cache to properly update the thread local on get_many

--- inyoka/utils/cache.py
+++ inyoka/utils/cache.py
@@ -5,10 +5,10 @@
 
     The caching infrastructure of Inyoka.
 
-    On top of the django cache client that speaks directly to either memcached or
-    caches in-memory we have a :class:`RequestCache` that caches memcached-commands
-    in a thread-local dictionary.  This saves a lot of memcached-commands in
-    some szenarios.
+    On top of the django cache client that speaks directly to either memcached
+    or caches in-memory we have a :class:`RequestCache` that caches
+    memcached-commands in a thread-local dictionary.  This saves a lot of
+    memcached-commands in some szenarios.
 
     :copyright: (c) 2007-2011 by the Inyoka Team, see AUTHORS for more details.
     :license: GNU GPL, see LICENSE for more details.
@@ -41,17 +41,18 @@ class RequestCache(BaseCache):
     def get_many(self, keys, version=None):
         if not local_has_key('cache'):
             return self.real_cache.get_many(keys, version)
-        keys = list(keys)
-
         key_mapping = {}
 
         # fetch keys that are not yet in the thread local cache
-        keys_to_fetch = set(key for key in keys if not key in self.request_cache)
+        keys_to_fetch = set(keys).difference(set(self.request_cache))
         key_mapping.update(self.real_cache.get_many(keys_to_fetch, version))
 
         # pull in remaining keys from thread local cache.
         missing = set(keys).difference(keys_to_fetch)
         key_mapping.update(dict((k, self.request_cache[k]) for k in missing))
+
+        # Update the request cache
+        self.request_cache.update(key_mapping)
         return key_mapping
 
     def set(self, key, value, timeout=None, version=None):
