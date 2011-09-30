797fabb1696bbf5935ede1f97b949a5790be0b06
Christopher Grebs <cg@webshox.org>
2011-09-30 13:31:39 +0200

    Isolate cache and storage tests better

--- inyoka/utils/tests/test_cache.py
+++ inyoka/utils/tests/test_cache.py
@@ -11,28 +11,32 @@ import time
 from django.test import TestCase
 from django.core.cache import cache
 
-from inyoka.utils.cache import request_cache, cache
+from inyoka.utils.cache import get_cache
 from inyoka.utils.local import local, _request_cache
 
 
-class TestStorage(TestCase):
+class TestCache(TestCase):
 
     def setUp(self):
         local.cache = {}
+        self.real = {}
+        self.cache = get_cache('default')
+        self.request_cache = get_cache('request')
+        self.request_cache.request_cache = self.real
 
     def test_set(self):
         def _compare(key, value, exists=True):
-            self.assertEqual(value, cache.get(key))
-            self.assertEqual(value, request_cache.get(key))
-            self.assertEqual(key in request_cache.request_cache, exists)
+            self.assertEqual(value, self.cache.get(key))
+            self.assertEqual(value, self.request_cache.get(key))
+            self.assertEqual(key in self.request_cache.request_cache, exists)
 
         def _compare_many(keys, value):
-            self.assertEqual(value, cache.get_many(keys))
-            self.assertEqual(value, request_cache.get_many(keys))
+            self.assertEqual(value, self.cache.get_many(keys))
+            self.assertEqual(value, self.request_cache.get_many(keys))
 
-        request_cache.set('test', 'foo')
-        request_cache.set('test', 'bar')
-        request_cache.set('bar', 'foo')
+        self.request_cache.set('test', 'foo')
+        self.request_cache.set('test', 'bar')
+        self.request_cache.set('bar', 'foo')
         _compare('test', 'bar')
         _compare('blah', None, False)
         _compare('bar', 'foo')
