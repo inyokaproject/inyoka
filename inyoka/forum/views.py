inyokaproject/inyoka

commit b1ecb9e06471ae0c2a0c32d5d961ccc1bd7201b0

Some more commits, just some intermediate result of my cleanup hacking...

inyoka/forum/views.py
@@ -1084,7 +1084,7 @@ def splittopic(request, topic_slug, page=1):
                      'new_topic': new_topic,
                      'old_topic': old_topic,
                      'mod': request.user.username}
-            users_done = set([request.user.id])
+            users_done = set(request.user.id)
             filter = Q(topic_id=old_topic.id)
             if data['action'] == 'new':
                 filter |= Q(forum_id=new_forum.id)

inyoka/wiki/models.py
@@ -279,7 +279,7 @@ class PageManager(models.Manager):
         of unicode strings, not the actual page object.  This ignores
         attachments!
         """
-        ignore = set([settings.WIKI_MAIN_PAGE])
+        ignore = set(settings.WIKI_MAIN_PAGE)
         pages = set(self.get_page_list())
         linked_pages = set(MetaData.objects.values_list('value', flat=True) \
                                            .filter(key='X-Link').all())

inyoka/middlewares/highlighter.py
@@ -43,7 +43,7 @@ _referrer_re = re.compile(
 
 _tag_re = re.compile(r'(<!--.*?-->|<[^>]*>)(?s)')
 _tagname_re = re.compile(r'<(/)?([a-zA-Z0-9]+)(?=\s|>)')
-_isolated = set(('script', 'head', 'style', 'textarea', 'option'))
+_isolated = frozenset(('script', 'head', 'style', 'textarea', 'option'))
 _umlaut_choices = [
     ('a',   ur'(?:a|ä)'),
     ('e',   ur'(?:e|é|è)'),

inyoka/scripts/clean_users.py
@@ -67,7 +67,7 @@ def get_inactive_users(excludes=None):
     current_datetime = datetime.fromtimestamp(time.time())
     delta = timedelta(days=settings.USER_INACTIVE_DAYS)
 
-    excludes = set(u.id for u in excludes) if excludes else set([])
+    excludes = set(u.id for u in excludes) if excludes else set()
 
     for user in User.objects.filter(status=1).exclude(id__in=excludes).all():
         if user.last_login and (current_datetime - user.last_login) < delta:

inyoka/scripts/generate_snapshot.py
@@ -93,7 +93,7 @@ def create_snapshot(folder):
     percents = list(percentize(len(todo)))
     pb = ProgressBar(40)
 
-    processed = set([])
+    processed = set()
 
     for percent, name in izip(percents, todo):
 

inyoka/scripts/planet_sync.py
@@ -31,7 +31,7 @@ import socket
 socket.setdefaulttimeout(20.0)
 
 
-HTML_MIMETYPES = set(['text/html', 'application/xml+xhtml', 'application/xhtml+xml'])
+HTML_MIMETYPES = frozenset(('text/html', 'application/xml+xhtml', 'application/xhtml+xml'))
 _par_re = re.compile(r'\n{2,}')
 
 

inyoka/utils/notification.py
@@ -66,7 +66,7 @@ def queue_notifications(request_user_id, template=None, subject=None, args=None,
     if exclude is not None:
         subscriptions = subscriptions.exclude(**exclude)
 
-    notified_users = set([])
+    notified_users = set()
 
     notified = set()
     for subscription in subscriptions.all():

inyoka/utils/urls.py
@@ -23,11 +23,11 @@ from django_hosts.reverse import get_host
 # extended at runtime with module introspection information
 _append_slash_map = {'static': False, 'media': False}
 _schema_re = re.compile(r'[a-z]+://')
-acceptable_protocols = set([
+acceptable_protocols = frozenset((
     'ed2k', 'ftp', 'http', 'https', 'irc', 'mailto', 'news', 'gopher',
     'nntp', 'telnet', 'webcal', 'xmpp', 'callto', 'feed', 'urn',
     'aim', 'rsync', 'tag', 'ssh', 'sftp', 'rtsp', 'afs', 'git', 'msn'
-])
+))
 
 
 def href(_module='portal', *parts, **query):
@@ -50,8 +50,8 @@ def href(_module='portal', *parts, **query):
             'static': settings.STATIC_URL,
         }[_module].rstrip('/')
     else:
-        subdomain = get_host(_module).regex
-        base_url = 'http://%s%s' % (subdomain + '.' if subdomain else '', settings.BASE_DOMAIN_NAME)
+        subdomain = get_host(_module).regex + ('.' if subdomain else '')
+        base_url = 'http://%s%s' % (subdomain, settings.BASE_DOMAIN_NAME)
 
     return '%s/%s%s%s%s' % (
         base_url,

commit 82178a6056d25874e928534dd23a6135be4f414b

fixed some regressions introduced by b1ecb9e06471ae0c2a0c32d5d961ccc1bd7201b0

inyoka/forum/views.py
@@ -1084,7 +1084,7 @@ def splittopic(request, topic_slug, page=1):
                      'new_topic': new_topic,
                      'old_topic': old_topic,
                      'mod': request.user.username}
-            users_done = set(request.user.id)
+            users_done = set([request.user.id])
             filter = Q(topic_id=old_topic.id)
             if data['action'] == 'new':
                 filter |= Q(forum_id=new_forum.id)

inyoka/wiki/models.py
@@ -279,7 +279,7 @@ class PageManager(models.Manager):
         of unicode strings, not the actual page object.  This ignores
         attachments!
         """
-        ignore = set(settings.WIKI_MAIN_PAGE)
+        ignore = set([settings.WIKI_MAIN_PAGE])
         pages = set(self.get_page_list())
         linked_pages = set(MetaData.objects.values_list('value', flat=True) \
                                            .filter(key='X-Link').all())