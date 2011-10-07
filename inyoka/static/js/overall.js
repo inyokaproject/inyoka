78b769d502018a3ce59e390e709efc0a9959e100
Christopher Grebs <cg@webshox.org>
2011-10-07 12:15:45 +0200

    Link to Oneiric Ocelot wikipage instead of ubuntu.com

--- inyoka/static/js/overall.js
+++ inyoka/static/js/overall.js
@@ -29,16 +29,16 @@ $(document).ready(function () {
     var days = Math.floor(diff / (1000*60*60*24))+1;
     var block = $("#counter")
     if (days > 21) {
-      block.html('<a href="http://www.ubuntu.com"><img src="' + $STATIC_URL + 'img/portal/countdown/oneiricbanner_start.png" alt="Ubuntu 11.10"></a>');
+      block.html('<a href="http://wiki.ubuntuusers.de/Oneiric_Ocelot"><img src="' + $STATIC_URL + 'img/portal/countdown/oneiricbanner_start.png" alt="Ubuntu 11.10"></a>');
     }
     if (days >= 1 && days<=21) {
-      block.html('<a href="http://www.ubuntu.com"><img src="' + $STATIC_URL + 'img/portal/countdown/oneiricbanner_' + days + '.png" alt="Ubuntu 11.10"></a>');
+      block.html('<a href="http://wiki.ubuntuusers.de/Oneiric_Ocelot"><img src="' + $STATIC_URL + 'img/portal/countdown/oneiricbanner_' + days + '.png" alt="Ubuntu 11.10"></a>');
     }
     if (days == 0) {
-      block.html('<a href="http://www.ubuntu.com"><img src="' + $STATIC_URL + 'img/portal/countdown/oneiricbanner_soon.png" alt="Ubuntu 11.10"></a>');
+      block.html('<a href="http://wiki.ubuntuusers.de/Oneiric_Ocelot"><img src="' + $STATIC_URL + 'img/portal/countdown/oneiricbanner_soon.png" alt="Ubuntu 11.10"></a>');
     }
     if (days < 0) {
-      block.html('<a href="http://www.ubuntu.com"><img src="' + $STATIC_URL + 'img/portal/countdown/oneiricbanner_here.png" alt="Ubuntu 11.10"></a>');
+      block.html('<a href="http://wiki.ubuntuusers.de/Oneiric_Ocelot"><img src="' + $STATIC_URL + 'img/portal/countdown/oneiricbanner_here.png" alt="Ubuntu 11.10"></a>');
     }
   })();
 
