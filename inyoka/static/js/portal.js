0e062d2b71bee8d7ad101cc5c986d39435dd5886
Markus Holtermann <markus@markusholtermann.eu>
2011-10-18 09:55:16 +0200

    make icons clickable if in edit mode.

--- inyoka/static/js/portal.js
+++ inyoka/static/js/portal.js
@@ -9,25 +9,50 @@
 */
 $(function () { 
     (function () {
+        function setFalse(element) {
+            element.addClass('dv-no').removeClass('dv-yes');
+        }
+
+        function setTrue(element) {
+            element.addClass('dv-yes').removeClass('dv-no');
+        }
+
+        var toggleBoolean = function (event) {
+            if ($(this).hasClass('dv-yes')) {
+                setFalse($(this));
+            } else if ($(this).hasClass('dv-no')) {
+                setTrue($(this));
+            }
+        }
+
+        function getStatus(element) {
+            if (element.hasClass('dv-yes')) {
+                return 'true';
+            } else if (element.hasClass('dv-no')) {
+                return 'false';
+            }
+            return null;
+        }
+
+        var keys = ['number', 'name', 'lts', 'active', 'current', 'dev'];
         var number_re = /^\d\d?\.\d\d$/;
         var revert = {};
-        //var editing = {}; //reserved for further use
+        var editing = {};
 
         var add_row = function (event) {
             event.preventDefault();
             var $row = $('<tr name="dv-new">' +
                 '<td><input type="text" name="dv-number"/></td>' +
                 '<td><input type="text" name="dv-name"/></td>' +
-                '<td><input type="checkbox" name="dv-lts"/></td>' +
-                '<td><input type="checkbox" name="dv-active"/></td>' +
-                '<td><input type="checkbox" name="dv-current"/></td>' +
-                '<td><input type="checkbox" name="dv-dev"/></td>' +
-                '<td></td>' +
                 '</tr>');
-            var $td_del = $('<td></td>');
+            for (var i = 2; i < keys.length; i++) {
+                $('<td></td>').attr('name', keys[i]).addClass('dv-no').click(toggleBoolean).appendTo($row);
+            }
+            $('<td></td>').appendTo($row);
+            var $e = $('<td></td>');
             var $a_del = $('<a href="#dv" name="dv-delete">Löschen</a>');
-            $a_del.click(delete_row).appendTo($td_del);
-            $td_del.appendTo($row);
+            $a_del.click(delete_row).appendTo($e);
+            $e.appendTo($row);
             $row.appendTo('#dv > tbody');
         };
 
@@ -40,6 +65,7 @@ $(function () {
             event.preventDefault();
             var $row = $(this).parent().parent();
             var version = $row.attr('id').substring(3);
+            // TODO: use keys to iterate over columns -> faster and more reliable
             $row.find("[name|=dv]").each(function () {
                 var $p = $(this).parent();
                 var key = $(this).attr('name').substr(3);
@@ -47,8 +73,12 @@ $(function () {
                     $p.attr('name', 'dv-' + key).empty();
                     $p.text(revert[version][key]);
                 } else if (key == 'lts' || key == 'active' || key == 'current' || key == 'dev') {
-                    $p.attr('name', 'dv-' + key).empty();
-                    $p.addClass('dv-' + (revert[version][key] ? 'yes' : 'no'));
+                    if (revert[version][key] == 'true') {
+                       setTrue($(this));
+                    } else if (revert[version][key] == 'false') {
+                        setFalse($(this));
+                    }
+                    $(this).unbind('click');
                 } else if (key == 'revert') {
                     $p.empty();
                     var $a_edit = $('<a href="#dv" name="dv-edit">Ändern</a>');
@@ -56,7 +86,7 @@ $(function () {
                 }
             });
             delete revert[version];
-            // delete editing[version];
+            delete editing[version];
         };
 
         var edit_row = function (event) {
@@ -64,6 +94,7 @@ $(function () {
             var $row = $(this).parent().parent();
             var version = $row.attr('id').substring(3); //strip the dv- from the version
             var dataset = new Object();
+            // TODO: use keys to iterate over columns -> faster and more reliable
             $row.find('[name^="dv"]').each(function () {
                 // iterate over all <td> elements.
                 var key = $(this).attr('name').substr(3);
@@ -74,20 +105,12 @@ $(function () {
                     $(this).removeAttr('name').empty();
                     $e.appendTo(this);
                 } else if (key == 'lts' || key == 'active' || key == 'current' || key == 'dev') {
-                    var $e = $('<input type="checkbox" name="dv-' + key + '"/>');
-                    if ($(this).hasClass('dv-yes')) {
-                        dataset[key] = 1;
-                        $e.attr('checked', 'checked');
-                        $e.val('true');
-                    } else {
-                        dataset[key] = 0;
-                    }
-                    $(this).removeAttr('name').empty();
-                    $e.appendTo(this);
+                    dataset[key] = getStatus($(this));
+                    $(this).addClass('pointer').click(toggleBoolean);
                 }
             });
             revert[version] = dataset;
-            //editing[version] = true;
+            editing[version] = true;
             var $p = $(this).parent();
             $(this).remove();
             var $a_revert = $('<a href="#dv" name="dv-revert">Abbrechen</a>');
@@ -102,21 +125,17 @@ $(function () {
 
         $('input[type="submit"]').click(function (event) {
             var distri_versions = new Array();
-            var keys = ['number', 'name', 'lts', 'active', 'current', 'dev'];
             $('tr[id|="dv"],tr[name="dv-new"]').each(function () {
                 var values = new Array(); //{number:'', name:'', lts:'', active:'', current:'', dev:''};
                 for (i = 0; i < keys.length; i++) {
                     var k = keys[i];
-                    var $e = $(this).find('[name|="dv-' + k +'"]');
+                    var $e = $(this).find('[name="dv-' + k +'"]');
                     var val;
                     if ($e.is('INPUT')) {
-                        if ($e.attr('type') == 'checkbox') {
-                            val = ($e.is(':checked')) ? 'true' : 'false';
-                        } else {
-                            val = $e.val();
-                        }
+                        val = $e.val();
                     } else {
-                        val = $e.text();
+                        val = getStatus($e);
+                        val = (val == null) ? $e.text() : val;
                     }
                     if (k == 'number' && !number_re.test(val) || k == 'name' && jQuery.trim(val) == '') {
                         return true; // return true to skip this loop but continue in `each()`
