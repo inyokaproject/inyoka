0b351d4d56660ac6bd962111785be645f1b42bfe
Markus Holtermann <markus@markusholtermann.eu>
2011-10-18 09:55:16 +0200

    make icons clickable if in edit mode.

--- inyoka/portal/forms.py
+++ inyoka/portal/forms.py
@@ -862,7 +862,7 @@ class ConfigurationForm(forms.Form):
     team_icon_height = forms.IntegerField(min_value=1, required=False)
     license_note = forms.CharField(required=False, label=u'Lizenzhinweis',
                                    widget=forms.Textarea(attrs={'rows': 2}))
-    distri_versions = forms.CharField(required=False)#, widget=HiddenInput())
+    distri_versions = forms.CharField(required=False, widget=HiddenInput())
 
     def clean_global_message(self):
         return cleanup_html(self.cleaned_data.get('global_message', ''))
