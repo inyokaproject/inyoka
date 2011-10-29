f51a09a65654b9322f9ccba42076c2859c7c9321
Christopher Grebs <cg@webshox.org>
2011-10-29 06:48:55 +0200

    Fake acitivty monitor not required as we now have rabbitmq

--- inyoka/tasks.py
+++ inyoka/tasks.py
@@ -6,11 +6,3 @@
     :copyright: (c) 2007-2011 by the Inyoka Team, see AUTHORS for more details.
     :license: GNU GPL, see LICENSE for more details.
 """
-from celery.task import periodic_task
-from celery.task.schedules import crontab
-
-
-@periodic_task(run_every=crontab(minute='*/1'))
-def activity_monitor():
-    """This task just does nothing but runs to get a simple monitoring"""
-    return
