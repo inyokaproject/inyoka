06dc6d0575a9026abef8e6a58eefba879e1ab890
Sören <wegener92@googlemail.com>
2011-11-13 18:18:25 +0100

    Added script to automatically fetch updates from Transifex.

---
+++ extra/translation_update.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+"""Fetches translations from Transifex and updates the local .po files"""
+
+from getpass import getpass
+import json
+import os
+from urllib import urlopen, urlretrieve
+
+LANGUAGES = ['de_DE']
+BASE_URL = 'http://www.transifex.net/api/2/project/inyoka'
+
+# I guess the following three lines can be done much nicer, but dunno how
+abs_path = os.path.abspath(__file__)
+extra_path = os.path.split(abs_path)[0]
+base_path = os.path.abspath('/'.join([extra_path, '..', 'inyoka']))
+
+print 'Transifex requires an authentication to use its API. Please type ' \
+      'in your login credentials below. They will not be saved and only be ' \
+      'used to fetch the latest translations from Transifex.'
+print
+resources = json.load(urlopen('/'.join([BASE_URL, 'resources'])))
+for resource in resources:
+    app = resource['slug']
+    for language in LANGUAGES:
+        file_path = '/'.join([base_path, app, 'locale', language, 'LC_MESSAGES',
+                             'django.po'])
+        url = '/'.join([BASE_URL, 'resource', app, 'translation', language]) + '?file'
+        dir_path = os.path.split(file_path)[0]
+        if not os.path.exists(dir_path):
+            os.makedirs(dirpath)
+        print 'Downloading "{0}"'.format(url)
+        urlretrieve(url, file_path)
+
+print 'Done, don´t forget to run manage.py compilemessages'
