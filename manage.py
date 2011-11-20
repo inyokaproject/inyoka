efd731a136d3d61855ddcd573f169991286f963e
Christopher Grebs <cg@webshox.org>
2011-11-20 19:59:04 +0100

    Updated manage.py to latest django code, to include sys.argv

--- manage.py
+++ manage.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
-import os
-
-from django.core import management
+import os, sys
 
 if __name__ == "__main__":
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'development_settings')
-    management.execute_from_command_line()
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "development_settings")
+
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
