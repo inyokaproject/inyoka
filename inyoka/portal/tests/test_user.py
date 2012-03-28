0c17c491029fb5a50279d7a906c35788472f890e
Sören <wegener92@googlemail.com>
2012-03-28 19:23:52 +0200

    Added unittest for "profile_data" property.

--- inyoka/portal/tests/test_user.py
+++ inyoka/portal/tests/test_user.py
@@ -9,24 +9,29 @@
 import unittest
 from datetime import datetime, timedelta
 from django.test import TestCase
-from inyoka.portal.user import User, Group, reactivate_user, deactivate_user
+from inyoka.portal.user import User, Group, ProfileData, ProfileField, \
+     ProfileCategory, reactivate_user, deactivate_user
 
 
 class TestUserModel(TestCase):
     def setUp(self):
         self.user = User.objects.register_user('testing', 'example@example.com',
                                                'pwd', False)
+        self.profile_category = ProfileCategory(title='Bar')
+        self.profile_field = ProfileField(title='Test',
+                                          category=self.profile_category)
+        self.profile_field.save()
 
     def test_reactivation(self):
         result = reactivate_user(self.user.id, '', '', datetime.utcnow())
-        self.assert_('failed' in result)
+        self.assertIn('failed', result)
         result = reactivate_user(self.user.id, '', '', datetime.utcnow() - timedelta(days=34))
-        self.assert_('failed' in result)
+        self.assertIn('failed', result)
         self.user.status = 3
         self.user.save()
         result = reactivate_user(self.user.id, 'example_new@example.com',\
                                  1, datetime.now())
-        self.assert_('success' in result)
+        self.assertIn('success', result)
         self.user = User.objects.get(pk=self.user.id)
         self.assertEqual(self.user.status, 1)
 
@@ -34,6 +39,14 @@ class TestUserModel(TestCase):
         deactivate_user(self.user)
         self.user = User.objects.get(pk=self.user.id)
         self.assertEqual(self.user.status, 3)
+
+    def test_profile_data(self):
+        field_data = ProfileData(user=self.user,
+                                 profile_field=self.profile_field,
+                                 data='Hello')
+        field_data.save()
+        data = ProfileData.objects.get(user=self.user)
+        self.assertEqual(data, self.user.profile_data.get(data='Hello'))
 
 
 class TestGroupModel(unittest.TestCase):
