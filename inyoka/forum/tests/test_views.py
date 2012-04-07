47e27d0f0906da90f3b4284d7c28c340e7c17365
Markus Holtermann <inyoka@markusholtermann.eu>
2012-04-08 01:57:40 +0200

    Changes as recommended in https://github.com/inyokaproject/inyoka/pull/57#issuecomment-5011192

--- inyoka/forum/tests/test_views.py
+++ inyoka/forum/tests/test_views.py
@@ -10,9 +10,6 @@ from inyoka.portal.user import User, PERMISSION_NAMES
 from inyoka.utils.test import InyokaClient
 
 
-@patch('inyoka.middlewares.security.SecurityMiddleware._make_token',
-        return_value = 'csrf_key')
-@patch('inyoka.utils.notification.send_notification')
 class TestViews(TestCase):
 
     client_class = InyokaClient
@@ -43,21 +40,25 @@ class TestViews(TestCase):
         self.client.defaults['HTTP_HOST'] = 'forum.%s' % settings.BASE_DOMAIN_NAME
         self.client.login(username='admin', password='admin')
 
-    def test_reported_topics(self, *args):
+    def test_reported_topics(self):
         response = self.client.get('/reported_topics/')
         self.assertEqual(response.status_code, 200)
 
+    @patch('inyoka.middlewares.security.SecurityMiddleware._make_token',
+            return_value = 'csrf_key')
+    @patch('inyoka.forum.views.send_notification')
     def test_movetopic(self, mock_send, mock_security):
         self.assertEqual(Topic.objects.get(id=self.topic.id).forum_id,
                 self.forum2.id)
         response = self.client.post('/topic/%s/move/' % self.topic.slug,
                     {'_form_token': 'csrf_key', 'forum': self.forum3.id})
         self.assertEqual(response.status_code, 302)
-        #mock_send.assert_called_with(self.user, 'topic_moved',
-        #    u'Your topic “Topic 1“ was moved.', {
-        #        'username': self.user.username, 'topic': self.topic,
-        #        'mod': self.admin.username, 'forum_name': 'Forum 3',
-        #        'old_forum_name': 'Forum 2'})
+        self.assertEqual(mock_send.call_count, 1) #only the topic author
+        mock_send.assert_called_with(self.user, 'topic_moved',
+            u'Your topic “A test Topic“ was moved.', {
+                'username': self.user.username, 'topic': self.topic,
+                'mod': self.admin.username, 'forum_name': 'Forum 3',
+                'old_forum_name': 'Forum 2'})
 
         self.assertEqual(Topic.objects.get(id=self.topic.id).forum_id,
                 self.forum3.id)
