36bc286e5e74947901c120c26ded3b901c95a328
Sören <wegener92@googlemail.com>
2012-04-13 20:54:00 +0200

    Added toggle capability to mobile forum categories.
    "Synchronizing" with server still missing.

--- inyoka/static/js/forum.m.js
+++ inyoka/static/js/forum.m.js
@@ -1,16 +1,28 @@
-/*
-static.js.forum.m
-~~~~~~~~~~~~~~~~~
-
-JavaScript for the mobile forum.
-
-:copyright: (c) 2007-2012 by the Inyoka Team, see AUTHORS for more details.
-:license: GNU GPL, see LICENSE for more details.
-*/
+/**
+ * static.js.forum.m
+ * ~~~~~~~~~~~~~~~~~
+ *
+ * JavaScript for the mobile forum.
+ *
+ * :copyright: (c) 2007-2012 by the Inyoka Team, see AUTHORS for more details.
+ * :license: GNU GPL, see LICENSE for more details.
+ */
 
 $(document).ready(function() {
   $('.attachments ul').hide()
   $('.attachments .title').click(function() {
     $(this).next('ul').toggle()
+  });
+
+  // expand and collapse button for categories
+  $('a.collapse').click(function() {
+    $(this).next().toggle();
+    // TODO: send the toggled categroy to the server and check for already
+    // hidden ones
+    /*$.get('/', {
+      __service__: 'forum.toggle_categories',
+      hidden:
+    }); */
+    return false;
   });
 });
