153a7d9cbe240c9b7ffc1a17b6813329af12942d
Phaiax <phaiax@invisibletower.de>
2012-05-23 21:05:29 +0200

    add untested tests

---
+++ inyoka/utils/tests/test_dates.py
@@ -0,0 +1,12 @@
+#-*- coding: utf-8 -*-
+import datetime
+from django.test import TestCase
+from inyoka.utils.dates import naturalday_with_adverb
+
+
+class TestDates(TestCase):
+
+    def test_naturalday(self):
+        self.assertIn('on', naturalday_with_adverb(datetime.date.today()))
+        self.assertNotIn('on', naturalday_with_adverb(datetime.date.today() +
+                                                      datetime.timedelta(days=2)))
