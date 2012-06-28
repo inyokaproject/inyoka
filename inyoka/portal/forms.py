590e43a8fa73f97cf9051fbb289c8481829edb25
Sören <wegener92@googlemail.com>
2012-06-28 21:37:53 +0200

    Minor language changes.

--- inyoka/portal/forms.py
+++ inyoka/portal/forms.py
@@ -877,7 +877,7 @@ class EditProfileFieldForm(forms.Form):
     important = forms.BooleanField(label=_('Important field'), help_text=_(
                                    u'”Imporant” fields are shown globally '
                                    u'on the whole website, for example '
-                                   u'beside every forum posts.'),
+                                   u'beside each forum posts.'),
                                    required=False)
 
     def clean(self):
