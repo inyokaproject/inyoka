398c1bec7c630ec682aaebf3616281e57bb6b4a3
Markus Holtermann <inyoka@markusholtermann.eu>
2012-09-15 00:31:27 +0200

    * Validate the CSRF token for the flash confirmation messages * Check for the confirm button be send

--- inyoka/utils/flash_confirmation.py
+++ inyoka/utils/flash_confirmation.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.utils.translation import ugettext as _
+from django.views.decorators.csrf import csrf_protect
+
 from inyoka.utils.decorators import patch_wrapper
 from inyoka.utils.http import HttpResponseRedirect, TemplateResponse
 from inyoka.utils.urls import href
@@ -8,16 +10,16 @@ from inyoka.utils.urls import href
 def confirm_action(message=None, confirm=None, cancel=None):
     """Decorator to render and handle "Are you sure?" flash messages.
 
-    Flashes a form to confirm an action via a POST request. Returns `True` if
-    the action was confirmed successfully.
-
+    Flashes a form to confirm an action via a POST request including CSRF
+    protection using :func:`django.views.decorators.csrf.csrf_protect`
     """
     def wrapper(func):
+        @csrf_protect
         def decorator(request, *args, **kwargs):
             if request.method == 'POST':
-                if 'cancel' in request.POST:
-                    return HttpResponseRedirect(href('portal'))
-                return func(request, *args, **kwargs)
+                if 'confirm' in request.POST:
+                    return func(request, *args, **kwargs)
+                return HttpResponseRedirect(href('portal'))
             else:
                 msg = message or _('Are you sure?')
                 confirm_label = confirm or _('Yes')
