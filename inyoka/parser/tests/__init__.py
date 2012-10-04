9dae5de975f047537f300cf1067417e3b4a34650
Florian Apolloner <florian@apolloner.eu>
2012-10-04 16:18:33 +0200

    Actually remove inyoka.parser :þ

--- inyoka/parser/tests/__init__.py
+++
@@ -1,6 +0,0 @@
-from inyoka.parser.tests.test_html_renderer import *
-from inyoka.parser.tests.test_lexer import *
-from inyoka.parser.tests.test_macros import *
-from inyoka.parser.tests.test_parser import *
-from inyoka.parser.tests.test_transformers import *
-from inyoka.parser.tests.test_css import *
