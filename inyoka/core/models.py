876027d155fb42af7e7de96f7982b9b3c6af67e6
Christopher Grebs <cg@webshox.org>
2012-10-13 08:18:07 +0200

    Moved autoimport logic for special modules to inyoka.core.models
    This now matches with the import mechanism of Django more or less
    perfectly as it does not import application models ahead of time
    and forces circular imports here.
    
    Still not perfect but without the app-refactor branch there seems
    to be no other way than that.

--- inyoka/core/models.py
+++ inyoka/core/models.py
@@ -1,7 +1,37 @@
 #-*- coding: utf-8 -*-
+from django.conf import settings
+from django.db.models import loading
+from django.utils.importlib import import_module
 
-# register core signals here as app.models get imported
-# by django on a proper point where we cannot mess up
-# the django.conf setup anymore.
 
-import inyoka.core.signals
+AUTOIMPORT_MODULES = ['search', 'macros']
+
+
+def register_special_modules():
+    """Import special modules for every application.
+
+    This enables to implement pluggable APIs where application
+    can register addons to some core functionality.
+
+    With this we are able to finally split application logic
+    from core functionality and cleanup some fancy circular imports.
+
+    This truely adds some overhead for the initial startup time
+    of Inyoka but as Python caches imports the overhead is bearable.
+    """
+    apps = loading.get_apps()
+    for app in apps:
+        name = app.__name__
+        if name.startswith('inyoka.'):
+            for path in AUTOIMPORT_MODULES:
+                module_path = '%s.%s' % ('.'.join(name.split('.')[:2]),
+                                         path)
+                try:
+                    import_module(module_path)
+                except ImportError, exc:
+                    _ = str(exc)
+                    if _ != 'No module named %s' % path:
+                        raise
+                    pass
+
+register_special_modules()
