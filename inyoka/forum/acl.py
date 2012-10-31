77d0678cab352922289fc72f4e6f309fec258cdb
Christopher Grebs <cg@webshox.org>
2012-10-31 16:39:33 +0100

    Fixed, clearified and updated post deletion tests

--- inyoka/forum/acl.py
+++ inyoka/forum/acl.py
@@ -217,5 +217,5 @@ filter_visible = lambda u, f=None, priv=CAN_READ, perm=None: filter(u, f, priv, 
 filter_invisible = lambda u, f=None, priv=CAN_READ, perm=None: filter(u, f, priv, perm, ops.ne)
 
 # circular imports
-from inyoka.forum.models import Privilege, Forum
 from inyoka.portal.user import DEFAULT_GROUP_ID
+from inyoka.forum.models import Privilege, Forum
