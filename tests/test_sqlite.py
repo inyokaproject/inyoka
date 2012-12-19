5738b7b8e2d189982a9f0866bb68491b3e15ee32
Christopher Grebs <cg@webshox.org>
2012-12-19 13:33:09 +0100

    explicit installed apps overwrite in tests config is not required anymore

--- tests/test_sqlite.py
+++ tests/test_sqlite.py
@@ -23,22 +23,5 @@ ADMIN_MEDIA_PREFIX = STATIC_URL + '/_admin/'
 INYOKA_SYSTEM_USER_EMAIL = 'system@' + BASE_DOMAIN_NAME
 GOOGLE_MAPS_APIKEY = 'ABQIAAAAnGRs_sYisCDW3FXIZAzZ9RR0WYmUN-JWdjE121Rerp-F3KIi4BQQM-N93TqupJwysf0dHBu_LfF6AQ'
 
-# Removed django-openid for now as we do not support proper test setup for now.
-# explicitly add inyoka.utils.tests to apps to run unittests here
-INSTALLED_APPS = (
-    'django.contrib.staticfiles',
-    'django.contrib.contenttypes',
-    'inyoka.core',
-    'inyoka.portal',
-    'inyoka.wiki',
-    'inyoka.forum',
-    'inyoka.ikhaya',
-    'inyoka.pastebin',
-    'inyoka.planet',
-    'tests.functional.utils',
-    'south',
-    # *must* be installed after south
-    'djcelery',
-    'djkombu',
-    'django_mobile',
-)
+# Explicitly add tests.functional.utils to apps to initialize models here
+INSTALLED_APPS = INSTALLED_APPS + ('tests.functional.utils',)
