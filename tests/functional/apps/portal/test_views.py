inyokaproject/inyoka

commit 40ad1579042f3e9ba710d54a549e84b515d66ab0

Update last_login via signals and try using Django's password reset views.

inyoka/default_settings.py
@@ -201,9 +201,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.humanize',
+    'inyoka.forum',
     'inyoka.portal',
     'inyoka.wiki',
-    'inyoka.forum',
     'inyoka.ikhaya',
     'inyoka.pastebin',
     'inyoka.planet',
@@ -323,6 +323,13 @@ PASSWORD_HASHERS = (
     'django.contrib.auth.hashers.SHA1PasswordHasher',
     'inyoka.utils.user.UnsaltedMD5PasswordHasher',
 )
+
+TEMPLATE_LOADERS = (
+    'inyoka.utils.templating.DjangoLoader',
+    'django.template.loaders.app_directories.Loader',
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = ()
 
 # export only uppercase keys
 __all__ = list(x for x in locals() if x.isupper())

inyoka/forum/__init__.py
@@ -6,5 +6,6 @@
     :copyright: (c) 2007-2012 by the Inyoka Team, see AUTHORS for more details.
     :license: GNU GPL, see LICENSE for more details.
 """
+# FIXME: Django 1.5 move to a sane location and add some loading stuff... (Circular imports :/)
 # register seach components
-from inyoka.forum import search
+#from inyoka.forum import search

inyoka/forum/acl.py
@@ -108,6 +108,8 @@ def get_forum_privileges(user, forum):
 
 
 def _get_privilege_map(user, forum_ids):
+    # circular imports
+    from inyoka.forum.models import Privilege, Forum
     group_ids = user.groups.values_list('id', flat=True)
 
     cols = ('forum__id', 'user__id', 'group__id', 'positive', 'negative')
@@ -217,5 +219,4 @@ filter_visible = lambda u, f=None, priv=CAN_READ, perm=None: filter(u, f, priv, 
 filter_invisible = lambda u, f=None, priv=CAN_READ, perm=None: filter(u, f, priv, perm, ops.ne)
 
 # circular imports
-from inyoka.forum.models import Privilege, Forum
 from inyoka.portal.user import DEFAULT_GROUP_ID

inyoka/ikhaya/models.py
@@ -596,56 +596,5 @@ class ArticleSearchAuthDecider(object):
         return self.priv or ((not auth[0]) and auth[1] <= self.now)
 
 
-class IkhayaSearchAdapter(SearchAdapter):
-    type_id = 'i'
-    auth_decider = ArticleSearchAuthDecider
-
-    def get_objects(self, docids):
-        return Article.objects.select_related(depth=1) \
-                      .filter(id__in=docids).all()
-
-    def store_object(self, article, connection=None):
-        search.store(connection,
-            component='i',
-            uid=article.id,
-            title=article.subject,
-            user=article.author_id,
-            date=article.pub_datetime,
-            auth=(article.hidden, article.pub_datetime),
-            category=article.category.slug,
-            text=[article.intro, article.text]
-        )
-
-    def extract_data(self, article):
-        return {'title': article.subject,
-                'user': article.author.username,
-                'date': article.pub_datetime,
-                'url': url_for(article),
-                'component': u'Ikhaya',
-                'group': article.category.name,
-                'group_url': url_for(article.category),
-                'highlight': True,
-                'text': u'%s %s' % (article.simplified_intro,
-                                    article.simplified_text),
-                'hidden': article.hidden,
-                'user_url': url_for(article.author)}
-
-    def recv(self, docid):
-        article = Article.objects.select_related(depth=1).get(id=docid)
-        return self.extract_data(article)
-
-    def recv_multi(self, docids):
-        articles = Article.objects.select_related(depth=1).filter(id__in=docids)
-        return [self.extract_data(article) for article in articles]
-
-    def get_doc_ids(self):
-        ids = Article.objects.values_list('id', flat=True).all()
-        for id in ids:
-            yield id
-
-
-#: Register search adapter
-search.register(IkhayaSearchAdapter())
-
 #: Register generic model signals
 from inyoka.utils import signals

inyoka/portal/forms.py
@@ -21,6 +21,8 @@ from django.core.validators import EMPTY_VALUES
 from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext_lazy, ugettext as _
 
+from django.contrib.auth.forms import PasswordResetForm
+
 from inyoka.forum.constants import SIMPLE_VERSION_CHOICES
 from inyoka.forum.acl import filter_invisible
 from inyoka.forum.forms import ForumField
@@ -193,35 +195,11 @@ class RegisterForm(forms.Form):
         return self.cleaned_data['email']
 
 
-class LostPasswordForm(forms.Form):
-    """
-    Form for the lost password form.
-
-    It's similar to the register form and uses
-    a hidden and a visible image CAPTCHA too.
-    """
-    username = forms.CharField(label=_('Username or email address'))
-    captcha = CaptchaField(label=_('CAPTCHA'))
-    hidden_captcha = HiddenCaptchaField(required=False)
-
-    def clean_username(self):
-        data = super(LostPasswordForm, self).clean()
-        if 'username' in data and '@' in data['username']:
-            try:
-                self.user = User.objects.get(email=data['username'])
-            except User.DoesNotExist:
-                raise forms.ValidationError(
-                    _(u'A user with the email address “%(mail)s“ does not exist.')
-                    % {'mail': data['username']}
-                )
-        else:
-            try:
-                self.user = User.objects.get(data['username'])
-            except User.DoesNotExist:
-                raise forms.ValidationError(
-                    _(u'The user “%(name)s“ does not exist.')
-                    % {'name': data['username']}
-                )
+class LostPasswordForm(PasswordResetForm):
+    def save(self, **opts):
+        request = opts['request']
+        messages.success(request, _(u'An email with further instructions was sent to you.'))
+        return super(PasswordResetForm, self).save(**opts)
 
 
 class SetNewPasswordForm(forms.Form):

inyoka/portal/templates/portal/lost_password.html
@@ -22,29 +22,10 @@
 		{{ csrf_token() }}
     <p class="hide"><input type="submit" value="Registrieren" /></p>
     {# enter -> first submit field #}
-    {{ render_form(form, ['username', 'email', 'captcha']) }}
+    {{ render_form(form, ['email']) }}
     <p>
       <input type="submit" value="{{ _('Request new password') }}" />
     </p>
 
   </form>
-{% endblock %}
-
-{% block additional_scripts %}
-  {{ super() }}
-  <script type="text/javascript">
-    /*<![CDATA[*/
-    (function() {
-      var captcha_url = $('img.captcha').attr('src');
-      $('input[name="renew_captcha"]')
-        .click(function() {
-          var uid = Math.random().toString().substring(2, 10);
-          $('img.captcha')
-            .attr('src', captcha_url + '&rnd=' + uid);
-          return false;
-        });
-      $('#id_username').focus();
-    })();
-    /*]]>*/
-  </script>
 {% endblock %}

inyoka/portal/urls.py
@@ -8,7 +8,7 @@
     :copyright: (c) 2007-2012 by the Inyoka Team, see AUTHORS for more details.
     :license: GNU GPL, see LICENSE for more details.
 """
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, url, include
 
 urlpatterns = patterns('inyoka.portal.views',
     (r'^$', 'index'),
@@ -60,8 +60,6 @@ urlpatterns = patterns('inyoka.portal.views',
     (r'^register/$', 'register'),
     (r'^(?P<action>activate|delete)/(?P<username>[^/]+)/(?P<activation_key>.*?)/$', 'activate'),
     (r'^register/resend/(?P<username>[^/]+)/$', 'resend_activation_mail'),
-    (r'^lost_password/$', 'lost_password'),
-    (r'^lost_password/(?P<username>[^/]+)/(?P<new_password_key>[a-z0-9]+)/$', 'set_new_password'),
     (r'^confirm/$', 'confirm'),
     (r'^confirm/(?P<action>[^/]+)/$', 'confirm'),
     (r'^feeds/$', 'feedselector'),
@@ -83,9 +81,6 @@ urlpatterns = patterns('inyoka.portal.views',
     (r'^files/(?P<slug>.+)/delete/$', 'file_delete'),
     (r'^pages/$', 'pages'),
     (r'^page/new/$', 'page_edit'),
-    (r'^([-A-Za-z_]+)/$', 'static_page'),
-    (r'^([-A-Za-z_]+)/edit/$', 'page_edit'),
-    (r'^(?P<pk>[-A-Za-z_]+)/delete/$', 'page_delete'),
 )
 
 
@@ -95,8 +90,19 @@ js_info_dict = {
 }
 
 urlpatterns += patterns('',
-    url(r'jsi18n/$', 'django.views.i18n.javascript_catalog', js_info_dict)
+    url(r'jsi18n/$', 'django.views.i18n.javascript_catalog', js_info_dict),
+    (r'^lost_password/$', 'inyoka.portal.views.lost_password'),
+    (r'^lost_password/(?P<username>[^/]+)/(?P<new_password_key>[a-z0-9]+)/$', 'set_new_password'),
+    (r'^reset_password/(?P<uidb36>[0-9A-Za-z]{1,13})-(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        'django.contrib.auth.views.password_reset_confirm'),
+    (r'^reset_password/done/$', 'django.contrib.auth.views.password_reset_complete'),
+
 )
 
+urlpatterns += patterns('inyoka.portal.views',
+    (r'^([-A-Za-z_]+)/$', 'static_page'),
+    (r'^([-A-Za-z_]+)/edit/$', 'page_edit'),
+    (r'^(?P<pk>[-A-Za-z_]+)/delete/$', 'page_delete'),
+)
 
 handler404 = 'inyoka.utils.urls.global_not_found'

inyoka/portal/views.py
@@ -283,30 +283,12 @@ def resend_activation_mail(request, username):
     return HttpResponseRedirect(href('portal'))
 
 
-@templated('portal/lost_password.html')
 def lost_password(request):
-    """
-    View for the lost password dialog.
-    It generates a new random password and sends it via mail.
-    """
-    if request.user.is_authenticated():
-        messages.error(request, _(u'You are already logged in.'))
-        return HttpResponseRedirect(href('portal'))
-
-    if request.method == 'POST':
-        form = LostPasswordForm(request.POST)
-        form.captcha_solution = request.session.get('captcha_solution')
-        if form.is_valid():
-            send_new_user_password(form.user)
-            messages.success(request, _(u'An email with further instructions was sent to you.'))
-            # clean up request.session
-            return HttpResponseRedirect(href('portal', 'login'))
-    else:
-        form = LostPasswordForm()
-
-    return {
-        'form': form
-    }
+    from django.contrib.auth.views import password_reset
+    return password_reset(request,
+        post_reset_redirect=href('portal', 'login'),
+        template_name='portal/lost_password.html',
+        password_reset_form=LostPasswordForm)
 
 
 @templated('portal/set_new_password.html')

commit aca0e722316d8597d71963f22ff8512e1a2c4e21

Two new test cases for lost password, changed mail adresses of user.
One test (test_lost_password_as_authenticated_user) fails on purpose,
but the currently production system shows this behaviour. Revision
40ad157 broke it.

tests/functional/apps/portal/test_views.py
@@ -62,7 +62,7 @@ class TestAuthViews(TestCase):
     client_class = InyokaClient
 
     def setUp(self):
-        self.user = User.objects.register_user('user', 'user', 'user', False)
+        self.user = User.objects.register_user('user', 'user@example.com', 'user', False)
         self.client.defaults['HTTP_HOST'] = settings.BASE_DOMAIN_NAME
 
     def test_valid_login(self):
@@ -180,7 +180,7 @@ class TestAuthViews(TestCase):
 
     def test_register(self):
         postdata = {'username': 'apollo13', 'password': 'secret',
-            'confirm_password': 'secret', 'email': 'apollo13@ubuntuusers.de',
+            'confirm_password': 'secret', 'email': 'apollo13@example.com',
             'terms_of_usage': '1'}
 
         self.assertEqual(0, len(mail.outbox))
@@ -189,3 +189,16 @@ class TestAuthViews(TestCase):
         self.assertEqual(1, len(mail.outbox))
         subject = mail.outbox[0].subject
         self.assertIn(u'Activation of the user “apollo13”', subject)
+
+    def test_lost_password(self):
+        postdata = {'email': 'user@example.com'}
+        with translation.override('en-us'):
+            response = self.client.post('/lost_password/', postdata)
+        subject = mail.outbox[0].subject
+        self.assertIn(u'New password for “user”', subject)
+
+    def test_lost_password_as_authenticated_user(self):
+        self.client.login(username='user', password='user')
+        with translation.override('en-us'):
+            response = self.client.get('/lost_password/', follow=True)
+        self.assertContains(response, 'You are already logged in.')