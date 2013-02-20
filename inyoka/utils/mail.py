28c058ab4088f560b63c4e8e87fe6f4618970ab4
Christopher Grebs <cg@webshox.org>
2013-02-20 12:00:37 +0100

    unused re import

--- inyoka/utils/mail.py
+++ inyoka/utils/mail.py
@@ -8,7 +8,6 @@
     :copyright: (c) 2007-2013 by the Inyoka Team, see AUTHORS for more details.
     :license: GNU GPL, see LICENSE for more details.
 """
-import re
 from subprocess import Popen, PIPE
 from django.conf import settings
 from django.core.mail import send_mail as django_send_mail
