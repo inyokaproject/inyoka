0db96fd39879d6e90a05ac8ca2151ecc0ab61605
Christopher Grebs <cg@webshox.org>
2013-04-09 16:36:46 +0200

    Remove ModelSearchIndex requirement, more efficient PostIndex

--- inyoka/forum/search_indexes.py
+++ inyoka/forum/search_indexes.py
@@ -4,32 +4,31 @@ from celery_haystack.indexes import CelerySearchIndex
 from inyoka.forum.models import Post
 
 
-class PostIndex(indexes.ModelSearchIndex, CelerySearchIndex, indexes.Indexable):
+class PostIndex(CelerySearchIndex, indexes.Indexable):
 
-    class Meta:
-        model = Post
-        fields = ['author', 'topic', 'topic_slug', 'pub_date', 'hidden',
-                  'text', 'has_revision', 'has_attachments', 'is_plaintext',
-                  'topic_auto']
+    _all = indexes.CharField(document=True, indexed=False, stored=False)
 
-    author = indexes.CharField(model_attr='author__username')
-    topic = indexes.CharField(model_attr='topic__title', boost=1.125)
-    topic_slug = indexes.CharField(model_attr='topic__slug')
+    author = indexes.CharField(model_attr='author__username', stored=False)
+    topic = indexes.CharField(model_attr='topic__title', boost=1.125, stored=False)
+    topic_slug = indexes.CharField(model_attr='topic__slug', stored=False)
 
-    ubuntu_version = indexes.CharField(model_attr='topic__ubuntu_version', null=True)
-    ubuntu_distro = indexes.CharField(model_attr='topic__ubuntu_distro', null=True)
+    ubuntu_version = indexes.CharField(model_attr='topic__ubuntu_version', null=True, stored=False)
+    ubuntu_distro = indexes.CharField(model_attr='topic__ubuntu_distro', null=True, stored=False)
 
-    pub_date = indexes.DateTimeField(model_attr='pub_date')
-    hidden = indexes.BooleanField(model_attr='hidden')
-    text = indexes.CharField(document=True)
-    has_revision = indexes.BooleanField(model_attr='has_revision')
-    has_attachments = indexes.BooleanField(model_attr='has_attachments')
+    pub_date = indexes.DateTimeField(model_attr='pub_date', stored=False)
+    hidden = indexes.BooleanField(model_attr='hidden', stored=False)
+    text = indexes.CharField(stored=False)
+    has_revision = indexes.BooleanField(model_attr='has_revision', stored=False)
+    has_attachments = indexes.BooleanField(model_attr='has_attachments', stored=False)
     is_plaintext = indexes.BooleanField(model_attr='is_plaintext', stored=False)
 
-    topic_auto = indexes.EdgeNgramField(model_attr='topic__title')
+    topic_auto = indexes.EdgeNgramField(model_attr='topic__title', stored=False)
 
     def prepare_text(self, obj):
         return obj.stripped_text
+
+    def get_model(self):
+        return Post
 
     def get_updated_field(self):
         return 'pub_date'
