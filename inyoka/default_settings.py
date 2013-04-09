94a4e9f099546e28bad33e0fb9aa6350f48b282d
Christopher Grebs <cg@webshox.org>
2013-04-09 16:38:45 +0200

    Silent is better for reindexing, need to set this True on production though

--- inyoka/default_settings.py
+++ inyoka/default_settings.py
@@ -355,7 +355,7 @@ HAYSTACK_CONNECTIONS = {
         'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
         'URL': 'http://127.0.0.1:9200/',
         'INDEX_NAME': 'inyoka',
-        'SILENTLY_FAIL': False
+        'SILENTLY_FAIL': True
     },
 }
 
