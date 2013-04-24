46910650105028add34328ea953d5d9e0cb7c038
Christopher Grebs <cg@webshox.org>
2013-04-24 13:43:06 +0200

    avoid side effects

--- inyoka/static/js/admin.js
+++ inyoka/static/js/admin.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
   (function () {
     if ($('textarea[name="signature"]').length !== 0) {
       // create a WikiEditor instance for all signature fields
-      var signature = new WikiEditor('textarea[name="signature"]');
+      new WikiEditor('textarea[name="signature"]');
     }
 
     // Small helper to define a users group title
