d90ec49ca1adc95b74675c2fad39f6718abeeaaf
Christopher Grebs <cg@webshox.org>
2013-04-24 14:29:54 +0200

    uglify was loaded twice

--- Gruntfile.js
+++ Gruntfile.js
@@ -41,7 +41,6 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
 
   // Default task(s).
   grunt.registerTask('default', ['uglify']);
