848e5c5147e8d3b565bd9fd4b2c9e5d63dc7af2a
Christopher Grebs <cg@webshox.org>
2013-04-24 15:25:12 +0200

    force memory sqlite db

--- tests/test_sqlite.py
+++ tests/test_sqlite.py
@@ -3,7 +3,7 @@ from inyoka.default_settings import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'test.db',
+        'NAME': ':memory:',
         'USER': '',
         'PASSWORD': '',
         'HOST': '',
