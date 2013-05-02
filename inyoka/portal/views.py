e689fe89cf52f2622c5b80dfa722aaa02f29ba86
Christopher Grebs <cg@webshox.org>
2013-05-02 17:52:42 +0200

    Removed unused imports

--- inyoka/portal/views.py
+++ inyoka/portal/views.py
@@ -10,8 +10,6 @@
     :copyright: (c) 2007-2013 by the Inyoka Team, see AUTHORS for more details.
     :license: GNU GPL, see LICENSE for more details.
 """
-import binascii
-import dateutil
 import pytz
 import time
 from PIL import Image
