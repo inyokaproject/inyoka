6e23457b21ac0dfd14d24f44dc24a8fadf9cb555
Christopher Grebs <cg@webshox.org>
2013-05-23 19:23:06 +0200

    Add 'grunt lint' alias

--- Gruntfile.js
+++ Gruntfile.js
@@ -47,4 +47,7 @@ module.exports = function(grunt) {
   // Default task(s).
   grunt.registerTask('default', ['uglify']);
 
+  // Alias jshint to lint
+  grunt.registerTask('lint', ['jshint']);
+
 };
