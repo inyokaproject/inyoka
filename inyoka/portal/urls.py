b372a39dc6ae497302dd3909d91540b0df87747f
Florian Apolloner <florian@apolloner.eu>
2013-05-26 23:57:54 +0200

    Stupid fix.

--- inyoka/portal/urls.py
+++ inyoka/portal/urls.py
@@ -8,7 +8,7 @@
     :copyright: (c) 2007-2013 by the Inyoka Team, see AUTHORS for more details.
     :license: GNU GPL, see LICENSE for more details.
 """
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, url, include
 
 urlpatterns = patterns('inyoka.portal.views',
     (r'^$', 'index'),
