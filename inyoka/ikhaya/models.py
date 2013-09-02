inyokaproject/inyoka

commit df73369b7a11c55f21464eb74a061a2735caa954

Ported Inyoka to handle XHTML and HTML properly and convert everything to HTML.
 * Renamed ikhaya.Article.is_xhtml to is_html
 * Rewrite CleanupFilter to work with lxml tree builder (syncs now with
   all live feeds properly)
 * Export feed content as html instead of xhtml
 * Updated docs

inyoka/ikhaya/models.py
@@ -211,7 +211,7 @@ class Article(models.Model, LockableObject):
     public = models.BooleanField(ugettext_lazy(u'Public'))
     slug = models.SlugField(ugettext_lazy(u'Slug'), max_length=100,
             blank=True, db_index=True)
-    is_xhtml = models.BooleanField(ugettext_lazy(u'XHTML Markup'),
+    is_html = models.BooleanField(ugettext_lazy(u'XHTML Markup'),
                 default=False)
     comment_count = models.IntegerField(default=0)
     comments_enabled = models.BooleanField(ugettext_lazy(u'Allow comments'),

inyoka/forum/views.py
@@ -1327,10 +1327,10 @@ def topic_feed(request, slug=None, mode='short', count=10):
         kwargs = {}
         if mode == 'full':
             kwargs['content'] = post.get_text()
-            kwargs['content_type'] = 'xhtml'
+            kwargs['content_type'] = 'html'
         if mode == 'short':
             kwargs['summary'] = Truncator(post.get_text).words(100, html=True)
-            kwargs['summary_type'] = 'xhtml'
+            kwargs['summary_type'] = 'html'
 
         feed.add(
             title='%s (%s)' % (
@@ -1383,10 +1383,10 @@ def forum_feed(request, slug=None, mode='short', count=10):
 
         if mode == 'full':
             kwargs['content'] = text
-            kwargs['content_type'] = 'xhtml'
+            kwargs['content_type'] = 'html'
         if mode == 'short':
             kwargs['summary'] = Truncator(text).words(100, html=True)
-            kwargs['summary_type'] = 'xhtml'
+            kwargs['summary_type'] = 'html'
 
         feed.add(
             title=topic.title,

inyoka/ikhaya/locale/de_DE/LC_MESSAGES/django.po
@@ -115,8 +115,8 @@ msgid "Public"
 msgstr "Veröffentlicht"
 
 #: inyoka/ikhaya/models.py:214
-msgid "XHTML Markup"
-msgstr "XHTML Markup"
+msgid "HTML Markup"
+msgstr "HTML Markup"
 
 #: inyoka/ikhaya/models.py:217
 msgid "Allow comments"

inyoka/ikhaya/locale/django.pot
@@ -103,7 +103,7 @@ msgid "Public"
 msgstr ""
 
 #: inyoka/ikhaya/models.py:214
-msgid "XHTML Markup"
+msgid "HTML Markup"
 msgstr ""
 
 #: inyoka/ikhaya/models.py:217

inyoka/ikhaya/migrations/0006_add_field_article_is_html.py
@@ -1,0 +1,135 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+
+        # Adding field 'Article.is_html'
+        db.add_column(u'ikhaya_article', 'is_html',
+                      self.gf('django.db.models.fields.BooleanField')(default=False),
+                      keep_default=False)
+
+    def backwards(self, orm):
+        # Deleting field 'Article.is_html'
+        db.delete_column(u'ikhaya_article', 'is_html')
+
+    models = {
+        u'ikhaya.article': {
+            'Meta': {'ordering': "['-pub_date', '-pub_time', 'author']", 'unique_together': "(('pub_date', 'slug'),)", 'object_name': 'Article'},
+            'author': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'article_set'", 'to': u"orm['portal.User']"}),
+            'category': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['ikhaya.Category']", 'on_delete': 'models.PROTECT'}),
+            'comment_count': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'comments_enabled': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'icon': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['portal.StaticFile']", 'null': 'True', 'on_delete': 'models.SET_NULL', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'intro': ('django.db.models.fields.TextField', [], {}),
+            'is_html': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_xhtml': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'pub_date': ('django.db.models.fields.DateField', [], {'db_index': 'True'}),
+            'pub_time': ('django.db.models.fields.TimeField', [], {}),
+            'public': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '100', 'blank': 'True'}),
+            'subject': ('django.db.models.fields.CharField', [], {'max_length': '180'}),
+            'text': ('django.db.models.fields.TextField', [], {}),
+            'updated': ('django.db.models.fields.DateTimeField', [], {'db_index': 'True', 'null': 'True', 'blank': 'True'})
+        },
+        u'ikhaya.category': {
+            'Meta': {'object_name': 'Category'},
+            'icon': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['portal.StaticFile']", 'null': 'True', 'on_delete': 'models.SET_NULL', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '180'}),
+            'slug': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'unique': 'True', 'max_length': '100', 'blank': 'True'})
+        },
+        u'ikhaya.comment': {
+            'Meta': {'object_name': 'Comment'},
+            'article': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['ikhaya.Article']", 'null': 'True'}),
+            'author': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['portal.User']"}),
+            'deleted': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'pub_date': ('django.db.models.fields.DateTimeField', [], {}),
+            'rendered_text': ('django.db.models.fields.TextField', [], {}),
+            'text': ('django.db.models.fields.TextField', [], {})
+        },
+        u'ikhaya.report': {
+            'Meta': {'object_name': 'Report'},
+            'article': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['ikhaya.Article']", 'null': 'True'}),
+            'author': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['portal.User']"}),
+            'deleted': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'pub_date': ('django.db.models.fields.DateTimeField', [], {}),
+            'rendered_text': ('django.db.models.fields.TextField', [], {}),
+            'solved': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'text': ('django.db.models.fields.TextField', [], {})
+        },
+        u'ikhaya.suggestion': {
+            'Meta': {'object_name': 'Suggestion'},
+            'author': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'suggestion_set'", 'to': u"orm['portal.User']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'intro': ('django.db.models.fields.TextField', [], {}),
+            'notes': ('django.db.models.fields.TextField', [], {'default': "u''", 'blank': 'True'}),
+            'owner': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'owned_suggestion_set'", 'null': 'True', 'to': u"orm['portal.User']"}),
+            'pub_date': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.utcnow'}),
+            'text': ('django.db.models.fields.TextField', [], {}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'portal.group': {
+            'Meta': {'object_name': 'Group'},
+            'icon': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_public': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80', 'db_index': 'True'}),
+            'permissions': ('django.db.models.fields.IntegerField', [], {'default': '0'})
+        },
+        u'portal.staticfile': {
+            'Meta': {'object_name': 'StaticFile'},
+            'file': ('django.db.models.fields.files.FileField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'identifier': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100', 'db_index': 'True'}),
+            'is_ikhaya_icon': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
+        },
+        u'portal.user': {
+            'Meta': {'object_name': 'User'},
+            '_permissions': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            '_primary_group': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'primary_users_set'", 'null': 'True', 'db_column': "'primary_group_id'", 'to': u"orm['portal.Group']"}),
+            'aim': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'avatar': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'banned_until': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'coordinates_lat': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            'coordinates_long': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.utcnow'}),
+            'email': ('django.db.models.fields.EmailField', [], {'unique': 'True', 'max_length': '50', 'db_index': 'True'}),
+            'forum_last_read': ('django.db.models.fields.IntegerField', [], {'default': '0', 'blank': 'True'}),
+            'forum_read_status': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'forum_welcome': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'gpgkey': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "'user_set'", 'blank': 'True', 'to': u"orm['portal.Group']"}),
+            'icq': ('django.db.models.fields.CharField', [], {'max_length': '16', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'interests': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'jabber': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'launchpad': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'location': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'member_title': ('django.db.models.fields.CharField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'msn': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'occupation': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'post_count': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'settings': ('django.db.models.TextField', [], {'default': '{}'}),
+            'signature': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'sip': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'skype': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'status': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30', 'db_index': 'True'}),
+            'website': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'}),
+            'wengophone': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'yim': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['ikhaya']

inyoka/ikhaya/migrations/0007_convert_is_xhtml_to_is_html.py
@@ -1,0 +1,136 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import DataMigration
+from django.db import models
+
+class Migration(DataMigration):
+
+    def forwards(self, orm):
+        "Write your forwards methods here."
+        for article in orm['ikhaya.Article'].objects.only('is_html', 'is_xhtml').all():
+            article.is_html = article.is_xhtml
+            article.save(update_fields=('is_html',))
+
+    def backwards(self, orm):
+        "Write your backwards methods here."
+        for article in orm['ikhaya.Article'].objects.only('is_html', 'is_xhtml').all():
+            article.is_html = article.is_xhtml
+            article.save(update_fields=('is_xhtml',))
+
+    models = {
+        u'ikhaya.article': {
+            'Meta': {'ordering': "['-pub_date', '-pub_time', 'author']", 'unique_together': "(('pub_date', 'slug'),)", 'object_name': 'Article'},
+            'author': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'article_set'", 'to': u"orm['portal.User']"}),
+            'category': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['ikhaya.Category']", 'on_delete': 'models.PROTECT'}),
+            'comment_count': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'comments_enabled': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'icon': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['portal.StaticFile']", 'null': 'True', 'on_delete': 'models.SET_NULL', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'intro': ('django.db.models.fields.TextField', [], {}),
+            'is_html': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_xhtml': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'pub_date': ('django.db.models.fields.DateField', [], {'db_index': 'True'}),
+            'pub_time': ('django.db.models.fields.TimeField', [], {}),
+            'public': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '100', 'blank': 'True'}),
+            'subject': ('django.db.models.fields.CharField', [], {'max_length': '180'}),
+            'text': ('django.db.models.fields.TextField', [], {}),
+            'updated': ('django.db.models.fields.DateTimeField', [], {'db_index': 'True', 'null': 'True', 'blank': 'True'})
+        },
+        u'ikhaya.category': {
+            'Meta': {'object_name': 'Category'},
+            'icon': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['portal.StaticFile']", 'null': 'True', 'on_delete': 'models.SET_NULL', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '180'}),
+            'slug': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'unique': 'True', 'max_length': '100', 'blank': 'True'})
+        },
+        u'ikhaya.comment': {
+            'Meta': {'object_name': 'Comment'},
+            'article': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['ikhaya.Article']", 'null': 'True'}),
+            'author': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['portal.User']"}),
+            'deleted': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'pub_date': ('django.db.models.fields.DateTimeField', [], {}),
+            'rendered_text': ('django.db.models.fields.TextField', [], {}),
+            'text': ('django.db.models.fields.TextField', [], {})
+        },
+        u'ikhaya.report': {
+            'Meta': {'object_name': 'Report'},
+            'article': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['ikhaya.Article']", 'null': 'True'}),
+            'author': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['portal.User']"}),
+            'deleted': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'pub_date': ('django.db.models.fields.DateTimeField', [], {}),
+            'rendered_text': ('django.db.models.fields.TextField', [], {}),
+            'solved': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'text': ('django.db.models.fields.TextField', [], {})
+        },
+        u'ikhaya.suggestion': {
+            'Meta': {'object_name': 'Suggestion'},
+            'author': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'suggestion_set'", 'to': u"orm['portal.User']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'intro': ('django.db.models.fields.TextField', [], {}),
+            'notes': ('django.db.models.fields.TextField', [], {'default': "u''", 'blank': 'True'}),
+            'owner': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'owned_suggestion_set'", 'null': 'True', 'to': u"orm['portal.User']"}),
+            'pub_date': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.utcnow'}),
+            'text': ('django.db.models.fields.TextField', [], {}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'portal.group': {
+            'Meta': {'object_name': 'Group'},
+            'icon': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_public': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80', 'db_index': 'True'}),
+            'permissions': ('django.db.models.fields.IntegerField', [], {'default': '0'})
+        },
+        u'portal.staticfile': {
+            'Meta': {'object_name': 'StaticFile'},
+            'file': ('django.db.models.fields.files.FileField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'identifier': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100', 'db_index': 'True'}),
+            'is_ikhaya_icon': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
+        },
+        u'portal.user': {
+            'Meta': {'object_name': 'User'},
+            '_permissions': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            '_primary_group': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'primary_users_set'", 'null': 'True', 'db_column': "'primary_group_id'", 'to': u"orm['portal.Group']"}),
+            'aim': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'avatar': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'banned_until': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'coordinates_lat': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            'coordinates_long': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.utcnow'}),
+            'email': ('django.db.models.fields.EmailField', [], {'unique': 'True', 'max_length': '50', 'db_index': 'True'}),
+            'forum_last_read': ('django.db.models.fields.IntegerField', [], {'default': '0', 'blank': 'True'}),
+            'forum_read_status': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'forum_welcome': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'gpgkey': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "'user_set'", 'blank': 'True', 'to': u"orm['portal.Group']"}),
+            'icq': ('django.db.models.fields.CharField', [], {'max_length': '16', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'interests': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'jabber': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'launchpad': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'location': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'member_title': ('django.db.models.fields.CharField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'msn': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'occupation': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'post_count': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'settings': ('django.db.models.TextField', [], {'default': '{}'}),
+            'signature': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'sip': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'skype': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'status': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30', 'db_index': 'True'}),
+            'website': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'}),
+            'wengophone': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'yim': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['ikhaya']
+    symmetrical = True

commit 68e036efb84fe12e6d7bca48870636973ed0e03e

Fix renaming of is_xhtml to is_html that has become a regression in df73369b7a

inyoka/ikhaya/models.py
@@ -236,7 +236,7 @@ class Article(models.Model, LockableObject):
 
     def _simplify(self, text):
         """Remove markup of a text that belongs to this Article"""
-        if self.is_xhtml:
+        if self.is_html:
             simple = striptags(text)
         else:
             simple = parse(text).text
@@ -244,7 +244,7 @@ class Article(models.Model, LockableObject):
 
     def _render(self, text):
         """Render a text that belongs to this Article to HTML"""
-        if self.is_xhtml:
+        if self.is_html:
             return text
         context = RenderContext(current_request, application='ikhaya')
         instructions = parse(text).compile('html')