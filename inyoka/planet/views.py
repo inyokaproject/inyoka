5cad10398ab4b9cd79cb5b0d0e855acd9dd1dd5b
Markus Holtermann <inyoka@markusholtermann.eu>
2013-10-18 13:48:19 +0200

    typo

--- inyoka/planet/views.py
+++ inyoka/planet/views.py
@@ -33,7 +33,7 @@ from inyoka.utils.pagination import Pagination
 
 def context_modifier(request, context):
     """
-    This function is called of ``templated`` automatically to copy the list of
+    This function is called by ``templated`` automatically to copy the list of
     blogs into the context.
     """
     context['blogs'] = Blog.objects.filter(active=True).all()
