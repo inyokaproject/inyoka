3898508600ec8d8d8822391eaeed4fb9e2a570a3
Soeren Wegener <wegener92@gmail.com>
2013-11-11 00:29:48 +0100

    Added collapse button for large codeblocks.

--- inyoka/themes/default/static/js/base.js
+++ inyoka/themes/default/static/js/base.js
@@ -26,11 +26,38 @@ $(function () {
     sidebar_resize();
     if (window.location.hash) {
         var pos = $(window.location.hash).offset().top - navbar_height;
-        window.setTimeout(function() {
-        $('html, body').animate({ scrollTop: pos }, 100);
+        window.setTimeout(function () {
+            $('html, body').animate({ scrollTop: pos }, 100);
         }, 50);
     }
 
     // initialize fancy tooltips
     $('[data-toggle="tooltip"]').tooltip();
+
+    // resizer for large codeblocks
+    $('div.code').add('pre.notranslate').each(function () {
+        if (this.clientHeight < this.scrollHeight) {
+            $(this).after('<button class="codeblock_resizer btn btn-default btn-sm">Expand</button>')
+                .css('height', '300px').css('max-height', 'none')
+                .data('original_height', this.clientHeight);
+        }
+    });
+
+    if (navigator.appName.toLowerCase() == 'konqueror') return;
+    $('.codeblock_resizer').click(function () {
+        $codeblock = $(this).prev();
+        if (!$codeblock.hasClass('codeblock_expanded')) {
+            $codeblock.addClass('codeblock_expanded');
+            $codeblock.animate({
+                'height': $codeblock[0].scrollHeight
+            }, 500);
+            $(this).text('Collapse');
+        } else {
+            $codeblock.removeClass('codeblock_expanded');
+            $codeblock.animate({
+                'height': $codeblock.data('original_height')
+            }, 500);
+            $(this).text('Expand');
+        }
+    });
 });
