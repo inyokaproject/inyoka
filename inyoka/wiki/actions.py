0026b0f9556945caafced7390942b0a0741de795
Philipp Schmidt <philschmidt@gmx.net>
2014-06-21 16:56:43 +0200

    use integrated link escape methods

--- inyoka/wiki/actions.py
+++ inyoka/wiki/actions.py
@@ -595,9 +595,13 @@ def do_mv_baustelle(request, name):
                 Page.objects.create(name=name, text=copy_text,
                     user=request.user,
                     note=u'Kopie; Original in der Baustelle')
-            
+
             messages.success(request,
-                u'Seite erfolgreich in die Baustelle verschoben - ggf. <a href="/Wiki/ACL/All-in-One">Zugriffsrechte</a> anpassen.')
+                u'Seite erfolgreich in die Baustelle verschoben - ggf. '
+                '<a href="%(link)s">Zugriffsrechte</a> anpassen' % {
+                    'link': escape(href('wiki', 'Wiki/ACL/All-in-One'))
+                }
+            )
             return HttpResponseRedirect(url_for(page))
 
     form = MvBaustelleForm()
@@ -702,7 +706,11 @@ def do_mv_back(request, name):
                 return HttpResponseRedirect(url_for(page))
 
             messages.success(request,
-                u'Seite erfolgreich ins Wiki verschoben - ggf. <a href="/Wiki/ACL/All-in-One">Zugriffsrechte</a> anpassen.')
+                u'Seite erfolgreich ins Wiki verschoben - ggf. '
+                '<a href="%(link)s">Zugriffsrechte</a> anpassen' % {
+                    'link': escape(href('wiki', 'Wiki/ACL/All-in-One'))
+                }
+            )
             return HttpResponseRedirect(url_for(page))
     else:
         messages.info(request,
