53246805ac197c61b30b3df2a00f0fb8595d5e61
Jens Gottfried <frustbox@gmail.com>
2014-08-19 21:23:58 +0200

    New Pagination -- The Class itself

--- inyoka/utils/pagination.py
+++ inyoka/utils/pagination.py
@@ -3,8 +3,10 @@
     inyoka.utils.pagination
     ~~~~~~~~~~~~~~~~~~~~~~~
 
-    This file helps creating a pagination. It's able to generate the HTML
-    source and to select the right database entries.
+    This file helps creating a pagination. It is able to select the right
+    database entries and return a subset of the query. The object has methods
+    and properties to retrieve the links to other pages, which can be used in
+    templates.
 
     Usage::
 
@@ -16,16 +18,33 @@
         ...     per_page=25, total=500)
         >>> # the database entries on this page
         >>> objects = pagination.get_queryset()
-        >>> # the generated HTML code for the pagination
-        >>> html = pagination.generate()
 
-    If the page is out of range, it throws a Http404 exception.
+    If the page is out of range, it throws a Http404 exception, when the object
+    is created.
     You can pass the optional argument `total` if you already know how
     many entries match your query. If you don't, `Pagination` will use
     a database query to find it out.
+    The number of pages can be limited with the optional argument `max_pages`.
     For tables that are quite big it's sometimes useful to use an indexed
     column determinating the position instead of using an offset / limit
     statement. In this case you can use the `rownum_column` argument.
+    To get all items on one page, set `one_page=True` or `per_page=0`.
+
+    URL to the first and last page will be accessible through `pagination.first`
+    and `pagination.last`.
+
+    Similarly `pagination.prev` and `pagination.next` will return the URL for the
+    previous or next page if there is such a page, otherwise return value will
+    be False.
+
+    `pagination.list(threshold)` is a generator that yields all URLs to be
+    displayed in order as well as necessary "spacers" if pages are omitted. The
+    parameter `threshold` determines how many pages are to be displayed
+    before/after current page and at beginning and end. Possible yields can take
+    three forms:
+        - `{ 'type': 'link', 'url': 'http://localhost/', 'page': 1 }`
+        - `{ 'type': 'current', 'url': 'http://localhost/2/', 'page': 2 }`
+        - `{ 'type': 'spacer' }`
 
     Caveat: paginations with link functions generated in a closure are
     not pickleable.
@@ -37,175 +56,168 @@ from __future__ import division
 
 import math
 
-from django.http import Http404, HttpResponseRedirect
+from django.http import Http404
 from django.utils.encoding import force_unicode
-from django.utils.html import escape
-from django.utils.translation import ugettext as _
 
-from django_mobile import get_flavour
 from inyoka.utils.urls import urlencode
 
 
 class Pagination(object):
+    """ Handle pagination """
 
-    def __init__(self, request, query, page, per_page=10, link=None,
-                 total=None, rownum_column=None):
-        self.page = int(page)
+    def __init__(self, request, query, page=1, per_page=10, link=None, total=None,
+                       rownum_column=None, max_pages=None, one_page=False):
+        """ Create pagination object
+
+            :param request: The current request.
+            :param query: The paginated objects, can be a list or tuple
+            :param page: Current page number.
+            :param per_page: Number of items per page.
+            :param link: The base url.
+            :param total: Total number of items in query, can be None.
+            :param rownum_column: Name of the column used to order items.
+            :param max_pages: Maximum number of pages.
+            :param one_page: If set, show all elements on one page
+        """
+
+        self.request       = request
+        self.query         = query
+        self.page          = int(page)
+        self.per_page      = int(per_page)
+        self.base_link     = self._get_base_link(link)
+        self.total         = self._get_total(total)
         self.rownum_column = rownum_column
-        self.per_page = per_page
-        self.query = query
 
-        if total:
-            self.total = total
-        elif isinstance(query, (list, tuple)):
-            self.total = len(query)
+        self._queryset = None
+        self._first    = None
+        self._last     = None
+        self._next     = None
+        self._prev     = None
+
+        if one_page or self.per_page < 0:
+            self.per_page = self.total
+            self.pages = 1
+            self.page = 1
         else:
-            self.total = query.count()
+            self.pages = max(0, (self.total-1)) // self.per_page + 1
 
-        if self.per_page == 0:
-            # Display all entries on one page
-            self.max_pages = 1
-        else:
-            self.max_pages = max(0, self.total - 1) // self.per_page + 1
+        if max_pages and self.pages > max_pages:
+            self.pages = max_pages
 
-        if self.page > self.max_pages:
+        if self.page > self.pages or self.page < 1:
             raise Http404()
-
-        if link is None:
-            link = request.path
-        self.parameters = request.GET
-        if isinstance(link, basestring):
-            self.link_base = link
-        else:
-            self.generate_link = link
-
-        self.needs_redirect_to = None
-        if self.total and self.total / per_page < 1 and page > 1:
-            url = self.generate_link(1, dict(request.GET.lists()))
-            self.needs_redirect_to = lambda: HttpResponseRedirect(url)
-
-    @property
-    def is_last_page(self):
-        return self.page == self.max_pages
-
-    @property
-    def has_objects(self):
-        return bool(max(0, self.total - 1) // self.per_page + 1)
-
-    def get_queryset(self):
-        idx = (self.page - 1) * self.per_page
-        if self.rownum_column:
-            expr = {'%s__gte' % self.rownum_column: idx,
-                    '%s__lt' % self.rownum_column: idx + self.per_page}
-            result = self.query.filter(**expr)
-        else:
-            result = self.query[idx:idx + self.per_page]
-        return result
-
-    def generate_link(self, page, params):
-        if page == 1:
-            url = self.link_base
-        else:
-            url = u'%s%d/' % (self.link_base, page)
-        return url + (params and u'?' + urlencode(params) or u'')
-
-    def generate(self, position=None, threshold=2, show_next_link=True,
-                 show_prev_link=True):
-        if get_flavour() == 'mobile':
-            return self.generate_mobile()
-        normal = u'<a href="%(href)s" class="pageselect">%(page)d</a>'
-        active = u'<span class="pageselect active">%(page)d</span>'
-        ellipsis = u'<span class="ellipsis"> … </span>'
-        was_ellipsis = False
-        result = []
-        add = result.append
-        pages = 1
-        if self.total:
-            pages = max(0, self.total - 1) // self.per_page + 1
 
         # This unicode/utf-8 conversion exists because of some fancy hacker-bots
         # that try to fuzz the pagination with some extremly invalid unicode data.
         # Catching those here fixes vulerabilty of the whole application.
         enc = lambda v: force_unicode(v).encode('utf-8') if isinstance(v, basestring) else v
-        params = {enc(k): enc(v) for k, v in self.parameters.iteritems()}
+        self.params = {enc(k): enc(v) for k, v in self.request.GET.iteritems()}
 
-        half_threshold = max(math.ceil(threshold / 2.0), 2)
-        for num in xrange(1, pages + 1):
-            if num <= threshold or num > pages - threshold or\
-               abs(self.page - num) < half_threshold:
-                if result and result[-1] != ellipsis:
-                    add(u'<span class="comma">, </span>')
+    def _get_base_link(self, link):
+        if link is None:
+            link = self.request.path
+        if isinstance(link, basestring):
+            return link
+        else:
+            self.generate_link = link
+
+    def _get_total(self, total):
+        if total:
+            return total
+        elif isinstance(self.query, (list,tuple)):
+            return len(self.query)
+        else:
+            return self.query.count()
+
+    def get_queryset(self):
+        """ Get objects for current page """
+
+        if self._queryset is not None:
+            return self._queryset
+
+        index_first = (self.page - 1) * self.per_page
+        index_last = index_first + self.per_page
+
+        if self.rownum_column:
+            expr = {'{}__gte'.format(self.rownum_column): index_first,
+                    '{}__lt'.format(self.rownum_column): index_last}
+            self._queryset = self.query.filter(**expr)
+        else:
+            self._queryset = self.query[index_first:index_last]
+
+        return self._queryset
+
+    def generate_link(self, page, params):
+        """ Get link for page number
+
+        :param page: page number
+        :return: A URL string
+        """
+
+        if page == 1:
+            url = self.base_link
+        else:
+            url = u'{}{}/'.format(self.base_link, page)
+        if self.params:
+            url = url + u'?{}'.format(urlencode(self.params))
+        return url
+
+    @property
+    def first(self):
+        """ Return the url to the first page """
+        if self._first is None:
+            self._first = self.generate_link(1, self.params)
+        return self._first
+
+    @property
+    def last(self):
+        """ Return the url to the last page """
+
+        if self._last is None:
+            self._last = self.generate_link(self.pages, self.params)
+        return self._last
+
+    @property
+    def prev(self):
+        """ Return the url to the previous page, or False if already on first page """
+
+        if self.page <= 1:
+            return False
+        if self._prev is None:
+            self._prev = self.generate_link(self.page-1, self.params)
+        return self._prev
+
+    @property
+    def next(self):
+        """ Return the url to the last page, or False if already on the last page """
+
+        if self.page >= self.pages:
+            return False
+        if self._next is None:
+            self._next = self.generate_link(self.page+1, self.params)
+        return self._next
+
+    def list(self, threshold=2):
+        """ Generator, lists all links for a pagination.
+
+        :param threshold: Number of pages displayed at beginning, end and
+                          before/after current page. Other pages are omitted.
+        :yields: A small dict containing at least a key `type` which can be 'link',
+                 'current' or 'spacer'. All except the 'spacer' will also have
+                 keys 'url' and 'page'.
+        """
+
+        for num in xrange(1,self.pages+1):
+            if num <= threshold or num > (self.pages-threshold) or \
+               abs(self.page - num) < threshold:
                 was_ellipsis = False
-                link = self.generate_link(num, params)
-                if num == self.page:
-                    template = active
-                else:
-                    template = normal
-                add(template % {
-                    'href': escape(link),
+                yield {
+                    'type': 'current' if self.page == num else 'link',
                     'page': num,
-                })
+                    'url': self.generate_link(num, self.params)
+                }
             elif not was_ellipsis:
                 was_ellipsis = True
-                add(ellipsis)
-
-        if show_next_link:
-            if self.page < pages:
-                link = self.generate_link(self.page + 1, params)
-                tmpl = u''.join([u'<a href="%s" class="next">',
-                                    _(u'Next »'), u'</a>'])
-                add(tmpl % escape(link))
-            else:
-                add(u''.join([u'<span class="disabled next">',
-                                    _(u'Next »'), u'</span>']))
-
-        if show_prev_link:
-            if self.page > 1:
-                link = self.generate_link(self.page - 1, params)
-                tmpl = u''.join([u'<a href="%s" class="prev">',
-                                    _(u'« Previous'), u'</a>'])
-                result.insert(0, tmpl % escape(link))
-            else:
-                result.insert(0, (u''.join([u'<span class="disabled prev">',
-                                    _(u'« Previous'), u'</span>'])))
-
-        class_ = 'pagination'
-        if position:
-            class_ += ' pagination_' + position
-        return u'<div class="%s">%s<div style="clear: both">' \
-               u'</div></div>' % (class_, u''.join(result))
-
-    def generate_mobile(self):
-        # This unicode/utf-8 conversion exists because of some fancy hacker-bots
-        # that try to fuzz the pagination with some extremly invalid unicode data.
-        # Catching those here fixes vulerabilty of the whole application.
-        _get_v = lambda v: force_unicode(v).encode('utf-8') if isinstance(v, basestring) else v
-
-        pages = 1
-        if self.total:
-            pages = max(0, self.total - 1) // self.per_page + 1
-
-        params = {force_unicode(k).encode('utf-8'): _get_v(v)
-                  for k, v in self.parameters.iteritems()}
-        s = u''.join([
-            u'<div class="pagination">',
-            u'<a href="{first_link}" class="prev first">«</a>' if self.page > 1 else '',
-            u'<a href="{prev_link}" class="prev">{prev_label}</a>' if self.page > 1 else '',
-            u'<span class="active">{current_label} {page} / {max_pages}</span>',
-            u'<a href="{next_link}" class="next">{next_label}</a>' if self.page < pages else '',
-            u'<a href="{last_link}" class="next last">»</a>' if self.page < pages else '',
-            u'<span style="display: none;" class="link_base">{link_base}</span>',
-            u'</div>',
-        ])
-        return s.format(
-            current_label=_(u'Page'),
-            first_link=self.generate_link(1, params),
-            last_link=self.generate_link(self.max_pages, params),
-            link_base=self.generate_link(1, params),
-            max_pages=self.max_pages,
-            next_label=_(u'Next »'),
-            next_link=self.generate_link(min(self.page + 1, self.max_pages), params),
-            prev_label=_(u'« Previous'),
-            prev_link=self.generate_link(max(self.page - 1, 1), params),
-            page=self.page
-        )
+                yield {
+                    'type': 'spacer'
+                }
