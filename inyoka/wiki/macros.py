inyokaproject/inyoka

commit 4dda4c0b137c37364a12d4bff7f330b6b2edf352

Fix CVE-2014-3730

inyoka/wiki/macros.py
@@ -28,7 +28,7 @@ from inyoka.utils.imaging import parse_dimensions
 from inyoka.utils.pagination import Pagination
 from inyoka.utils.templating import render_template
 from inyoka.utils.text import get_pagetitle, join_pagename, normalize_pagename
-from inyoka.utils.urls import href, url_for, urlencode, is_external_target
+from inyoka.utils.urls import href, url_for, urlencode, is_safe_domain
 from inyoka.wiki.models import Page, MetaData, Revision
 from inyoka.wiki.signals import build_picture_node
 from inyoka.wiki.views import fetch_real_target
@@ -716,15 +716,15 @@ class Attachment(macros.Macro):
     def __init__(self, target, text):
         self.target = target
         self.text = text
-        self.is_external = is_external_target(target)
-        if not self.is_external:
+        self.is_internal = is_safe_domain(target)
+        if self.is_internal:
             self.metadata = [nodes.MetaData('X-Attach', [target])]
             target = normalize_pagename(target, True)
         self.children = [nodes.Text(self.text or self.target)]
 
     def build_node(self, context, format):
         target = self.target
-        if self.is_external:
+        if not self.is_internal:
             return nodes.Link(target, self.children)
         else:
             wiki_page = context.kwargs.get('wiki_page', None)

inyoka/utils/imaging.py
@@ -15,7 +15,7 @@ from PIL import Image
 from django.conf import settings
 from django.utils.encoding import force_unicode
 
-from inyoka.utils.urls import is_external_target
+from inyoka.utils.urls import is_safe_domain
 
 
 def _get_box(img, width, height):
@@ -90,7 +90,7 @@ def get_thumbnail(location, destination, width=None, height=None, force=False):
     if not width and not height:
         raise ValueError('neither with nor height given')
 
-    if is_external_target(location):
+    if not is_safe_domain(location):
         return None
 
     format, quality = ('png', '100')

inyoka/utils/urls.py
@@ -92,18 +92,33 @@ def url_for(obj, action=None, **kwargs):
 
 
 def is_safe_domain(url):
-    """Check whether `url` points to the same host as inyoka"""
+    """
+    Check whether `url` points to the same host as inyoka
+
+    Return ``True`` if the url is a safe redirection (i.e. it doesn't point to
+    a different host and uses a safe scheme).
+    Always returns ``False`` on an empty url.
+
+    Adopted from :func:`django.utils.http.is_safe_url` and related to
+    CVE-2014-3730
+    """
     if not url:
         return False
-    scheme, netloc = urlparse(url)[:2]
-    if scheme not in acceptable_protocols:
+
+    # Chrome treats \ completely as /
+    url = url.replace('\\', '/')
+
+    # Chrome considers any URL with more than two slashes to be absolute, but
+    # urlaprse is not so flexible. Treat any url with three slashes as unsafe.
+    if url.startswith('///'):
         return False
-    return ('.' + netloc).endswith('.' + settings.BASE_DOMAIN_NAME)
+    scheme, netloc = urlparse(url)[:2]
 
-
-def is_external_target(location):
-    """
-    Check if a target points to an external URL or an internal page.  Returns
-    `True` if the target is an external URL.
-    """
-    return _schema_re.match(location) is not None
+    # Forbid URLs like http:///example.com - with a scheme, but without a hostname.
+    # In that URL, example.com is not the hostname but, a path component. However,
+    # Chrome will still consider example.com to be the hostname, so we must not
+    # allow this syntax.
+    if not netloc and scheme:
+        return False
+    return ((not netloc or ('.' + netloc).endswith('.' + settings.BASE_DOMAIN_NAME)) and
+            (not scheme or scheme in acceptable_protocols))

commit 549509a91e3172a371f8f51fc4f262b87efc6759

Fix regression introduced in 4dda4c0b13 due to old database content

inyoka/wiki/macros.py
@@ -716,15 +716,15 @@ class Attachment(macros.Macro):
     def __init__(self, target, text):
         self.target = target
         self.text = text
-        self.is_internal = is_safe_domain(target)
-        if self.is_internal:
+        self.is_external = not is_safe_domain(target)
+        if not self.is_external:
             self.metadata = [nodes.MetaData('X-Attach', [target])]
             target = normalize_pagename(target, True)
         self.children = [nodes.Text(self.text or self.target)]
 
     def build_node(self, context, format):
         target = self.target
-        if not self.is_internal:
+        if self.is_external:
             return nodes.Link(target, self.children)
         else:
             wiki_page = context.kwargs.get('wiki_page', None)