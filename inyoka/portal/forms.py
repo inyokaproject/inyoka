028584c02d27c4798cd3320891d6f4268bc46c89
Pyfisch <pyfisch@gmail.com>
2015-02-16 20:59:22 +0100

    Fix lost password test.

--- inyoka/portal/forms.py
+++ inyoka/portal/forms.py
@@ -18,10 +18,15 @@ from django import forms
 from django.conf import settings
 from django.forms import HiddenInput
 from django.contrib import messages
+from django.contrib.sites.models import get_current_site
 from django.db.models import Count
 from django.core.cache import cache
-from django.contrib.auth import forms as auth_forms
+from django.contrib.auth import forms as auth_forms, get_user_model
+from django.contrib.auth.tokens import default_token_generator
 from django.core.files.base import ContentFile
+from django.template import loader
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
 from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext as _
 from django.utils.translation import ugettext_lazy
@@ -163,10 +168,50 @@ class RegisterForm(forms.Form):
 
 
 class LostPasswordForm(auth_forms.PasswordResetForm):
-    def save(self, **opts):
-        request = opts['request']
+    def save(self, domain_override=None,
+             subject_template_name='registration/password_reset_subject.txt',
+             email_template_name='registration/password_reset_email.html',
+             use_https=False, token_generator=default_token_generator,
+             from_email=None, request=None):
+        """
+        Generates a one-use only link for resetting password and sends to the
+        user.
+        """
+        # FIXME: Copied from stock Django. Django believes is_active to be a
+        # field in the database, but it is no one in our data model.
+        from django.core.mail import send_mail
         messages.success(request, _(u'An email with further instructions was sent to you.'))
-        return super(LostPasswordForm, self).save(**opts)
+        UserModel = get_user_model()
+        email = self.cleaned_data["email"]
+        active_users = UserModel._default_manager.filter(
+            email__iexact=email)
+        for user in active_users:
+            # Make sure that no email is sent to a user that actually has
+            # a password marked as unusable
+            if not user.is_active:
+                continue
+            if not user.has_usable_password():
+                continue
+            if not domain_override:
+                current_site = get_current_site(request)
+                site_name = current_site.name
+                domain = current_site.domain
+            else:
+                site_name = domain = domain_override
+            c = {
+                'email': user.email,
+                'domain': domain,
+                'site_name': site_name,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'user': user,
+                'token': token_generator.make_token(user),
+                'protocol': 'https' if use_https else 'http',
+            }
+            subject = loader.render_to_string(subject_template_name, c)
+            # Email subject *must not* contain newlines
+            subject = ''.join(subject.splitlines())
+            email = loader.render_to_string(email_template_name, c)
+            send_mail(subject, email, from_email, [user.email])
 
 
 class SetNewPasswordForm(auth_forms.SetPasswordForm):
