5614e7d1bee1695edea4a94834080e187bf6eb6d
Jonatan Zeidler <jonatan_zeidler@gmx.de>
2015-08-03 12:47:46 -0300

    Use django.core.managment to run management command

--- tests/apps/portal/test_admin.py
+++ tests/apps/portal/test_admin.py
@@ -8,23 +8,19 @@
     :copyright: (c) 2007-2015 by the Inyoka Team, see AUTHORS for more details.
     :license: BSD, see LICENSE for more details.
 """
-from inyoka.portal.management.commands.renameusers import Command
 from inyoka.portal.user import User
 
 from django.test import TestCase
+from django.core import management
 import StringIO
-from sys import stderr, stdout
 
 
 class TestAdminCommands(TestCase):
 
     def test_rename_users(self):
-        User.objects.register_user('gandalf', 'gandalf@hdr.de', 'pwd', False)
-        User.objects.register_user('saroman', 'saroman@hdr.de', 'pwd', False)
+        User.objects.register_user("gandalf", "gandalf@hdr.de", "pwd", False)
+        User.objects.register_user("saroman", "saroman@hdr.de", "pwd", False)
         output = StringIO.StringIO('[{"oldname":"gandalf", "newname":"thewhite"},{"oldname":"saroman", "newname":"thedead"}]')
-        com = Command()
-        com.stdout = stdout  # FIXME: Workaround for a Django 1.5 bug. Should be removed after shifting to newer Django Version
-        com.stderr = stderr  # FIXME: Workaround for a Django 1.5 bug. Should be removed after shifting to newer Django Version
-        com.handle(*[output, "silent"], **{})
-        self.assertEqual(User.objects.get_by_username_or_email("gandalf@hdr.de").__unicode__(), "thewhite")
-        self.assertEqual(User.objects.get_by_username_or_email("saroman@hdr.de").__unicode__(), "thedead")
+        management.call_command("renameusers", *[output, "silent"])
+        self.assertEqual(unicode(User.objects.get_by_username_or_email("gandalf@hdr.de")), "thewhite")
+        self.assertEqual(unicode(User.objects.get_by_username_or_email("saroman@hdr.de")), "thedead")
