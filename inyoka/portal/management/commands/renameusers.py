e0f702d6967896ca64bb0bea52a48cbac862e447
Jonatan Zeidler <jonatan_zeidler@gmx.de>
2015-08-07 10:13:50 -0300

    command line uutputs respect verbosity level

--- inyoka/portal/management/commands/renameusers.py
+++ inyoka/portal/management/commands/renameusers.py
@@ -6,7 +6,7 @@
     This module provides a command to the Django ``manage.py`` file to rename
     a list of users defined by a JSON file as shown in the following example:
     [{"oldname":"user1", "newname":"kloss"},{"oldname":"user2", "newname":"spinne"}]
-    If the optional parameter 'silent' is passed, no mails will be sent to the users
+    If the optional parameter '--silent' or '-s' is passed, no mails will be sent to the users
     :copyright: (c) 2011-2015 by the Inyoka Team, see AUTHORS for more details.
     :license: BSD, see LICENSE for more details.
 """
@@ -31,6 +31,7 @@ class Command(LabelCommand):
 
     def handle_label(self, label, **options):
         notify = options.get('notify')
+        verbosity = int(options.get('verbosity'))
         if isinstance(label, basestring):
             with open(label) as json_file:
                 data = json.load(json_file)
@@ -38,6 +39,8 @@ class Command(LabelCommand):
             data = json.load(label)
         for username in data:
             try:
+                if verbosity >= 2:
+                    self.stdout.write(_(u"Renaming {oldname} to {newname}...").format(oldname=username["oldname"], newname=username["newname"]))
                 user = User.objects.get_by_username_or_email(username["oldname"])
             except User.DoesNotExist:
                 self.stderr.write(_(u"User '{oldname}' does not exist. Skipping...").format(oldname=username["oldname"]))
@@ -47,4 +50,5 @@ class Command(LabelCommand):
                         self.stderr.write(_(u"User name '{newname}' already exists. Skipping...").format(newname=username["newname"]))
                 except ValueError:
                     self.stderr.write(_(u"New user name '{newname}' contains invalid characters. Skipping...").format(newname=username["newname"]))
-        self.stdout.write(_(u"Renaming users complete."))
+        if verbosity >= 1:
+            self.stdout.write(_(u"Renaming users complete."))
