35e7194052b36507822301d5df8f4477acb73118
Oskar Hahn <mail@oshahn.de>
2015-10-29 11:27:16 +0100

    rebase

--- inyoka/wiki/migrations/0003_remove_text_html_render_instructions_old.py
+++ inyoka/wiki/migrations/0003_remove_text_html_render_instructions_old.py
@@ -7,7 +7,7 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('wiki', '0001_initial'),
+        ('wiki', '0002_auto_20151027_2210'),
     ]
 
     operations = [
