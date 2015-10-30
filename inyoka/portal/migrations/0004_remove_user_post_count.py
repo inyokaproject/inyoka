684bba9eb7d71c0726cceb5fb61bf18938c065b1
Oskar Hahn <mail@oshahn.de>
2015-10-30 20:10:45 +0100

    Add migrations

---
+++ inyoka/portal/migrations/0004_remove_user_post_count.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('portal', '0003_auto_20151029_1233'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='user',
+            name='post_count',
+        ),
+    ]
