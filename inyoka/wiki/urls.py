9479abad37e16f0886bd7eecef771ab975c84462
Oskar Hahn <mail@oshahn.de>
2015-12-17 08:19:36 +0100

    Updated all urls.py files to the new format.
    The documentation of django recommands to use the url function to define
    urls.
    
    Besides django changed the format of the urlpattern object that is expected
    in the urls.py module. The support for the old format will be dropped in
    django 1.10.
    
    Now, the second argument of the url function has to be a callable.
    
    See https://docs.djangoproject.com/en/1.9/topics/http/urls/
    
    I fixed the most of it with this regular expression:
    ^(\s*)(?:url|)(\(r'.*?',\s*)'(.*?)'

--- inyoka/wiki/urls.py
+++ inyoka/wiki/urls.py
@@ -9,64 +9,66 @@
     :license: BSD, see LICENSE for more details.
 """
 from django.conf import settings
-from django.conf.urls import include, patterns, url
+from django.conf.urls import include, url
 
-urlpatterns = patterns('inyoka.wiki.views',
-    (r'^$', 'index'),
-    (r'^_image$', 'get_image_resource'),
-    (r'^_newpage$', 'redirect_new_page'),
-    (r'^_attachment$', 'get_attachment'),
-    (r'^_feed/(?P<count>\d+)/?$', 'feed'),
-    (r'^(?P<page_name>.+)/a/feed/?$', 'feed', {'count': 10}),
-    (r'^(?P<page_name>.+)/a/feed/(?P<count>\d+)/?$', 'feed'),
-    (r'(?i)^wiki/recentchanges', 'recentchanges'),
-    (r'(?i)^wiki/missingpages', 'missingpages'),
-    (r'(?i)^wiki/tagcloud$', 'show_tag_cloud'),
-    (r'(?i)^wiki/tags$', 'show_tag_list'),
-    (r'(?i)^wiki/tags/(?P<tag>.+)', 'show_pages_by_tag')
-    )
+from . import actions, views
+
+urlpatterns = [
+    url(r'^$', views.index),
+    url(r'^_image$', views.get_image_resource),
+    url(r'^_newpage$', views.redirect_new_page),
+    url(r'^_attachment$', views.get_attachment),
+    url(r'^_feed/(?P<count>\d+)/?$', views.feed),
+    url(r'^(?P<page_name>.+)/a/feed/?$', views.feed, {'count': 10}),
+    url(r'^(?P<page_name>.+)/a/feed/(?P<count>\d+)/?$', views.feed),
+    url(r'(?i)^wiki/recentchanges', views.recentchanges),
+    url(r'(?i)^wiki/missingpages', views.missingpages),
+    url(r'(?i)^wiki/tagcloud$', views.show_tag_cloud),
+    url(r'(?i)^wiki/tags$', views.show_tag_list),
+    url(r'(?i)^wiki/tags/(?P<tag>.+)', views.show_pages_by_tag)
+]
 
 if settings.DEBUG:
     import debug_toolbar
-    urlpatterns += patterns('',
+    urlpatterns.append(
         url(r'^__debug__/', include(debug_toolbar.urls)),
     )
 
-urlpatterns += patterns('inyoka.wiki.actions',
-    (r'(?i)^wiki/create/?$', 'do_create'),
-    (r'(?i)^wiki/create/(?P<name>.+)/?$', 'do_create'),
-    (r'^(?P<name>.+)/a/attachments/?$', 'do_attach'),
-    (r'^(?P<name>.+)/a/backlinks/?$', 'do_backlinks'),
-    (r'^(?P<name>.+)/a/delete/?$', 'do_delete'),
-    (r'^(?P<name>.+)/a/diff/(?P<old_rev>\d+)/(?P<new_rev>\d+)/?$', 'do_diff'),
-    (r'^(?P<name>.+)/a/diff/(?P<old_rev>\d+)/?$', 'do_diff'),
-    (r'^(?P<name>.+)/a/diff/?$', 'do_diff'),
-    (r'^(?P<name>.+)/a/discussion/?$', 'do_manage_discussion'),
-    (r'^(?P<name>.+)/a/edit/?$', 'do_edit'),
-    (r'^(?P<name>.+)/a/edit/revision/(?P<rev>\d+)/?$', 'do_edit'),
-    (r'^(?P<name>.+)/a/export/(?P<format>(raw|html|ast))/?$', 'do_export'),
-    (r'^(?P<name>.+)/a/export/(?P<format>(raw|html|ast))/fragment/?$', 'do_export', {'fragment': True}),
-    (r'^(?P<name>.+)/a/export/(?P<format>(raw|html|ast))/(?P<rev>\d+)/?$', 'do_export'),
-    (r'^(?P<name>.+)/a/export/(?P<format>(raw|html|ast))/(?P<rev>\d+)/fragment/?$', 'do_export', {'fragment': True}),
-    (r'^(?P<name>.+)/a/export/meta/?$', 'do_metaexport'),
-    (r'^(?P<name>.+)/a/log/(?P<pagination_page>\d+)/?$', 'do_log'),
-    (r'^(?P<name>.+)/a/log/?$', 'do_log'),
-    (r'^(?P<name>.+)/a/mv_back/?$', 'do_mv_back'),
-    (r'^(?P<name>.+)/a/mv_baustelle/?$', 'do_mv_baustelle'),
-    (r'^(?P<name>.+)/a/mv_discontinued/?$', 'do_mv_discontinued'),
-    (r'^(?P<name>.+)/a/rename/?$', 'do_rename'),
-    (r'^(?P<name>.+)/a/rename/force/?$', 'do_rename', {'force': True}),
-    (r'^(?P<name>.+)/a/revert/(?P<rev>\d+)/?$', 'do_revert'),
-    (r'^(?P<name>.+)/a/revision/(?P<rev>\d+)/?$', 'do_show'),
-    (r'^(?P<name>.+)/a/revision/(?P<rev>\d+)/no_redirect/?$', 'do_show', {'allow_redirect': False}),
-    (r'^(?P<name>.+)/a/subscribe/?$', 'do_subscribe'),
-    (r'^(?P<name>.+)/a/udiff/(?P<old_rev>\d+)/(?P<new_rev>\d+)/?$', 'do_diff', {'udiff': True}),
-    (r'^(?P<name>.+)/a/udiff/(?P<old_rev>\d+)/?$', 'do_diff', {'udiff': True}),
-    (r'^(?P<name>.+)/a/udiff/?$', 'do_diff', {'udiff': True}),
-    (r'^(?P<name>.+)/a/unsubscribe/?$', 'do_unsubscribe'),
-    (r'^(?P<name>.+)/no_redirect/?$', 'do_show', {'allow_redirect': False}),
-    (r'^(?P<name>.+)/?$', 'do_show'),
-    )
+urlpatterns.extend([
+    url(r'(?i)^wiki/create/?$', actions.do_create),
+    url(r'(?i)^wiki/create/(?P<name>.+)/?$', actions.do_create),
+    url(r'^(?P<name>.+)/a/attachments/?$', actions.do_attach),
+    url(r'^(?P<name>.+)/a/backlinks/?$', actions.do_backlinks),
+    url(r'^(?P<name>.+)/a/delete/?$', actions.do_delete),
+    url(r'^(?P<name>.+)/a/diff/(?P<old_rev>\d+)/(?P<new_rev>\d+)/?$', actions.do_diff),
+    url(r'^(?P<name>.+)/a/diff/(?P<old_rev>\d+)/?$', actions.do_diff),
+    url(r'^(?P<name>.+)/a/diff/?$', actions.do_diff),
+    url(r'^(?P<name>.+)/a/discussion/?$', actions.do_manage_discussion),
+    url(r'^(?P<name>.+)/a/edit/?$', actions.do_edit),
+    url(r'^(?P<name>.+)/a/edit/revision/(?P<rev>\d+)/?$', actions.do_edit),
+    url(r'^(?P<name>.+)/a/export/(?P<format>(raw|html|ast))/?$', actions.do_export),
+    url(r'^(?P<name>.+)/a/export/(?P<format>(raw|html|ast))/fragment/?$', actions.do_export, {'fragment': True}),
+    url(r'^(?P<name>.+)/a/export/(?P<format>(raw|html|ast))/(?P<rev>\d+)/?$', actions.do_export),
+    url(r'^(?P<name>.+)/a/export/(?P<format>(raw|html|ast))/(?P<rev>\d+)/fragment/?$', actions.do_export, {'fragment': True}),
+    url(r'^(?P<name>.+)/a/export/meta/?$', actions.do_metaexport),
+    url(r'^(?P<name>.+)/a/log/(?P<pagination_page>\d+)/?$', actions.do_log),
+    url(r'^(?P<name>.+)/a/log/?$', actions.do_log),
+    url(r'^(?P<name>.+)/a/mv_back/?$', actions.do_mv_back),
+    url(r'^(?P<name>.+)/a/mv_baustelle/?$', actions.do_mv_baustelle),
+    url(r'^(?P<name>.+)/a/mv_discontinued/?$', actions.do_mv_discontinued),
+    url(r'^(?P<name>.+)/a/rename/?$', actions.do_rename),
+    url(r'^(?P<name>.+)/a/rename/force/?$', actions.do_rename, {'force': True}),
+    url(r'^(?P<name>.+)/a/revert/(?P<rev>\d+)/?$', actions.do_revert),
+    url(r'^(?P<name>.+)/a/revision/(?P<rev>\d+)/?$', actions.do_show),
+    url(r'^(?P<name>.+)/a/revision/(?P<rev>\d+)/no_redirect/?$', actions.do_show, {'allow_redirect': False}),
+    url(r'^(?P<name>.+)/a/subscribe/?$', actions.do_subscribe),
+    url(r'^(?P<name>.+)/a/udiff/(?P<old_rev>\d+)/(?P<new_rev>\d+)/?$', actions.do_diff, {'udiff': True}),
+    url(r'^(?P<name>.+)/a/udiff/(?P<old_rev>\d+)/?$', actions.do_diff, {'udiff': True}),
+    url(r'^(?P<name>.+)/a/udiff/?$', actions.do_diff, {'udiff': True}),
+    url(r'^(?P<name>.+)/a/unsubscribe/?$', actions.do_unsubscribe),
+    url(r'^(?P<name>.+)/no_redirect/?$', actions.do_show, {'allow_redirect': False}),
+    url(r'^(?P<name>.+)/?$', actions.do_show),
+])
 
 handler404 = 'inyoka.utils.http.global_not_found'
 require_trailing_slash = False
