113ceac1cbc13296a9e6b27363844bd3ef0c41b4
Christoph Volkert <inyoka@christoph-volkert.de>
2016-02-26 20:44:50 +0100

    Added little migrations for changed help_text

---
+++ inyoka/ikhaya/migrations/0004_auto_20160226_2043.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('ikhaya', '0003_auto_20151028_1857'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='article',
+            name='slug',
+            field=models.SlugField(help_text='Unique URL-part for the article. If not given, the slug will be generated from title.', max_length=100, verbose_name='Slug', blank=True),
+        ),
+    ]
