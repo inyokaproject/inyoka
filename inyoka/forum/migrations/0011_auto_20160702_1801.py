04bb56b51e7d471c737a2aeac4b0f389fd34af3e
Stefan J. Betz <info@stefan-betz.net>
2016-07-02 18:09:10 +0200

    Moved Forum Group Relations to Django Groups.

---
+++ inyoka/forum/migrations/0011_auto_20160702_1801.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('forum', '0010_auto_20160106_1247'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='forum',
+            name='support_group',
+            field=models.ForeignKey(related_name='forums', on_delete=django.db.models.deletion.SET_NULL, verbose_name='Support group', blank=True, to='auth.Group', null=True),
+        ),
+        migrations.AlterField(
+            model_name='privilege',
+            name='group',
+            field=models.ForeignKey(to='auth.Group', null=True),
+        ),
+    ]
