a71db273d7ac142be54fafa688ff7712fbb21bfc
Stefan J. Betz <info@stefan-betz.net>
2016-07-03 02:24:24 +0200

    Ported pastebin to django permissions.

--- inyoka/pastebin/models.py
+++ inyoka/pastebin/models.py
@@ -32,6 +32,9 @@ class Entry(models.Model):
         verbose_name = ugettext_lazy('Entry')
         verbose_name_plural = ugettext_lazy('Entries')
         ordering = ('-id',)
+        permissions = (
+            ('view_entry', 'View Entry'),
+        )
 
     @property
     def referrer_list(self):
