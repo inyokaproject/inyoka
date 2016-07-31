11b92bc77daebb7ac637fd3a03a3c3c278ab4037
Stefan J. Betz <info@stefan-betz.net>
2016-07-31 03:00:13 +0200

    First attempt to implemente Django Form based Permission handling.

--- inyoka/portal/views.py
+++ inyoka/portal/views.py
@@ -49,6 +49,8 @@ from inyoka.portal.forms import (
     EditUserGroupsForm,
     EditUserProfileForm,
     EditUserStatusForm,
+    GroupGlobalPermissionForm,
+    GroupForumPermissionForm,
     ForumFeedSelectorForm,
     IkhayaFeedSelectorForm,
     LoginForm,
@@ -1289,17 +1291,21 @@ def group_edit(request, name):
 
     if request.method == 'POST':
         form = EditGroupForm(request.POST, instance=group)
-        if form.is_valid():
+        global_permission_form = GroupGlobalPermissionForm(request.POST)
+        if form.is_valid() and global_permission_form.is_valid():
             group = form.save()
+            global_permission_form.save()
             messages.success(request,
                 _(u'The group “%s” was changed successfully.') % group.name)
             return HttpResponseRedirect(href('portal', 'group', group.name, 'edit'))
     else:
         form = EditGroupForm(instance=group)
+        global_permission_form = GroupGlobalPermissionForm(instance=Group)
 
     return {
         'form': form,
         'group': group,
+        'global_permission_form': global_permission_form,
     }
 
 
