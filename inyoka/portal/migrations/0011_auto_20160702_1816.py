bf7a7dd22ccc8cbc0e4138b355f2b234b3ae554b
Stefan J. Betz <info@stefan-betz.net>
2016-08-14 23:16:49 +0200

    Delete Database Indexes for old User=>Group Relation.
    This is a workaround, as Django created duplicate indexes if a column name is reused. Fixes #587

--- inyoka/portal/migrations/0011_auto_20160702_1816.py
+++ inyoka/portal/migrations/0011_auto_20160702_1816.py
@@ -3,6 +3,10 @@ from __future__ import unicode_literals
 
 from django.db import migrations, models
 
+def postgresql_drop_indexes(apps, schema_editor):
+    if schema_editor.connection.vendor.startswith('postgre'):
+        schema_editor.execute('DROP INDEX portal_user_groups_e8701ad4;')
+        schema_editor.execute('DROP INDEX portal_user_groups_0e939a4f;')
 
 class Migration(migrations.Migration):
 
@@ -16,4 +20,5 @@ class Migration(migrations.Migration):
             old_name='groups',
             new_name='groups_old',
         ),
+        migrations.RunPython(postgresql_drop_indexes),
     ]
