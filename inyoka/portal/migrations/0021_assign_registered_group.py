ddd5deb6f0072a81d6d8a970164d63865cbb23b5
Stefan J. Betz <info@stefan-betz.net>
2016-08-15 15:14:52 +0200

    Ensure that all active users are always in the registered group.
    This means for make_testdata if status==1 we assign the registered
    group. On production systems we use an migration via RunSQL() for it.
    
    A RAW SQL is needed, because iterating over ordinary python objects is
    really slow (several minutes, on postgresql without fsync). The RAW query
    only needs a few seconds to complete.

---
+++ inyoka/portal/migrations/0021_assign_registered_group.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.conf import settings
+from django.db import migrations
+
+
+def assign_registered_group(apps, schema_editor):
+    anonymous_id = apps.get_model('portal', 'User').objects.get(username=settings.ANONYMOUS_USER_NAME).id
+    registered_id = apps.get_model('auth', 'Group').objects.get(name=settings.INYOKA_REGISTERED_GROUP_NAME).id
+    schema_editor.execute("INSERT INTO portal_user_groups (user_id, group_id) SELECT id, %s FROM portal_user WHERE status = 1 AND NOT id = %s;", [registered_id, anonymous_id])
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('guardian', '0001_initial'),
+        ('portal', '0020_system_groups_and_users'),
+    ]
+
+    operations = [
+        migrations.RunPython(assign_registered_group),
+    ]
