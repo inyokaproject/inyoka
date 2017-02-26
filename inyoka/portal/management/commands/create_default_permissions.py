637c838ce238cb908fc2a7eacea3e541c031f247
Philipp Schmidt <philipp@schmidt-rheinhausen.de>
2017-02-26 11:35:16 +0100

    Default permissions for all team groups

--- inyoka/portal/management/commands/create_default_permissions.py
+++ inyoka/portal/management/commands/create_default_permissions.py
@@ -37,16 +37,16 @@ class Command(BaseCommand):
         team_group = Group.objects.get(name=settings.INYOKA_TEAM_GROUP_NAME)
         group_names = ["Projektleitung","Moderatoren","Supporter","Webteam","Ikhayateam","Serverteam","Wikiteam"]
         for group_name in group_names:
-            self.stdout.write("Assigning team perms for group %s" % (group_name))
             group = Group.objects.get(name=group_name)
-            assign_perm("portal.send_group_privatemessage", group)
             
             self.stdout.write("Adding users from group %s to group %s" % (group_name, team_group.name))
             for user in group.user_set.all():
                 self.stdout.write("    %s" % (user.username))
                 user.groups.add(team_group)
-                #team_group.objects.add(user)
             
+        self.stdout.write("Assigning team perms")
+        assign_perm("portal.send_group_privatemessage", team_group)
+        
         
         group_names = ["Projektleitung","Moderatoren","Webteam"]
         for group_name in group_names:
@@ -63,12 +63,112 @@ class Command(BaseCommand):
             assign_perm("auth.add_group", group)
             assign_perm("auth.change_group", group)
             assign_perm("portal.add_user", group)
-            assign_perm("ikhaya.delete_article", group)
+            
+            assign_perm("ikhaya.add_article", group)
+            assign_perm("ikhaya.add_category", group)
+            assign_perm("ikhaya.add_comment", group)
+            assign_perm("ikhaya.add_report", group)
+            assign_perm("ikhaya.add_suggestion", group)
+            assign_perm("ikhaya.change_article", group)
+            assign_perm("ikhaya.change_category", group)
+            assign_perm("ikhaya.change_comment", group)
+            assign_perm("ikhaya.change_report", group)
             assign_perm("ikhaya.change_suggestion", group)
+            assign_perm("ikhaya.delete_article", group)
+            assign_perm("ikhaya.delete_category", group)
+            assign_perm("ikhaya.delete_comment", group)
+            assign_perm("ikhaya.delete_report", group)
             assign_perm("ikhaya.delete_suggestion", group)
+            assign_perm("ikhaya.suggest_article", group)
+            assign_perm("ikhaya.view_unpublished_article", group)
             assign_perm("portal.add_event", group)
+            assign_perm("portal.change_event", group)
+            assign_perm("portal.delete_event", group)
+            assign_perm("portal.suggest_event", group)
+            assign_perm("planet.add_blog", group)
+            assign_perm("planet.change_blog", group)
+            assign_perm("planet.delete_blog", group)
+            assign_perm("planet.suggest_blog", group)
+            assign_perm("planet.view_blog", group)
+            assign_perm("planet.add_entry", group)
+            assign_perm("planet.change_entry", group)
+            assign_perm("planet.delete_entry", group)
+            assign_perm("planet.hide_entry", group)
             
+            assign_perm("wiki.add_attachment", group)
+            assign_perm("wiki.change_attachment", group)
+            assign_perm("wiki.delete_attachment", group)
+            assign_perm("wiki.add_metadata", group)
+            assign_perm("wiki.change_metadata", group)
+            assign_perm("wiki.delete_metadata", group)
+            assign_perm("wiki.add_page", group)
+            assign_perm("wiki.change_page", group)
+            assign_perm("wiki.delete_page", group)
+            assign_perm("wiki.add_revision", group)
+            assign_perm("wiki.change_revision", group)
+            assign_perm("wiki.delete_revision", group)
+            assign_perm("wiki.add_text", group)
+            assign_perm("wiki.change_text", group)
+            assign_perm("wiki.delete_text", group)
             
+            assign_perm("forum.change_forum", group)
+        
+        
+        # Permissions for all remaining teams
+        
+        ikhayateam = Group.objects.get(name="Ikhayateam")
+        assign_perm("ikhaya.add_article", ikhayateam)
+        assign_perm("ikhaya.add_category", ikhayateam)
+        assign_perm("ikhaya.add_comment", ikhayateam)
+        assign_perm("ikhaya.add_report", ikhayateam)
+        assign_perm("ikhaya.add_suggestion", ikhayateam)
+        assign_perm("ikhaya.change_article", ikhayateam)
+        assign_perm("ikhaya.change_category", ikhayateam)
+        assign_perm("ikhaya.change_comment", ikhayateam)
+        assign_perm("ikhaya.change_report", ikhayateam)
+        assign_perm("ikhaya.change_suggestion", ikhayateam)
+        assign_perm("ikhaya.delete_article", ikhayateam)
+        assign_perm("ikhaya.delete_category", ikhayateam)
+        assign_perm("ikhaya.delete_comment", ikhayateam)
+        assign_perm("ikhaya.delete_report", ikhayateam)
+        assign_perm("ikhaya.delete_suggestion", ikhayateam)
+        assign_perm("ikhaya.suggest_article", ikhayateam)
+        assign_perm("ikhaya.view_unpublished_article", ikhayateam)
+        assign_perm("portal.add_event", ikhayateam)
+        assign_perm("portal.change_event", ikhayateam)
+        assign_perm("portal.delete_event", ikhayateam)
+        assign_perm("portal.suggest_event", ikhayateam)
+        assign_perm("planet.add_blog", ikhayateam)
+        assign_perm("planet.change_blog", ikhayateam)
+        assign_perm("planet.delete_blog", ikhayateam)
+        assign_perm("planet.suggest_blog", ikhayateam)
+        assign_perm("planet.view_blog", ikhayateam)
+        assign_perm("planet.add_entry", ikhayateam)
+        assign_perm("planet.change_entry", ikhayateam)
+        assign_perm("planet.delete_entry", ikhayateam)
+        assign_perm("planet.hide_entry", ikhayateam)
+          
+        wikiteam = Group.objects.get(name="Wikiteam")
+        assign_perm("wiki.add_attachment", wikiteam)
+        assign_perm("wiki.change_attachment", wikiteam)
+        assign_perm("wiki.delete_attachment", wikiteam)
+        assign_perm("wiki.add_metadata", wikiteam)
+        assign_perm("wiki.change_metadata", wikiteam)
+        assign_perm("wiki.delete_metadata", wikiteam)
+        assign_perm("wiki.add_page", wikiteam)
+        assign_perm("wiki.change_page", wikiteam)
+        assign_perm("wiki.delete_page", wikiteam)
+        assign_perm("wiki.add_revision", wikiteam)
+        assign_perm("wiki.change_revision", wikiteam)
+        assign_perm("wiki.delete_revision", wikiteam)
+        assign_perm("wiki.add_text", wikiteam)
+        assign_perm("wiki.change_text", wikiteam)
+        assign_perm("wiki.delete_text", wikiteam)
+        
+        moderatorentl = Group.objects.get(name="Teamleitung-Moderatoren")
+        assign_perm("forum.change_forum", moderatorentl)
+        
+       
         # registered global perms
         
         self.stdout.write("Assigning default perms for group registered")
@@ -81,9 +181,12 @@ class Command(BaseCommand):
         # default forum permissions
         
         forum_id_list = [5,6,7,8,10,13,14,18,20,23,26,27,28,29,33,36,46,47,48,51,52,53,54,56,57,58,60,61,63,66,67,68,69,70,72,73,74,76,77,79,86,87,89,92,93,98]
+        
         anonymous = Group.objects.get(name=settings.INYOKA_ANONYMOUS_GROUP_NAME)
         
+        self.stdout.write("Adding permissions for anonymous to view paste entries and suggest events")
         assign_perm("pastebin.view_entry", anonymous)
+        assign_perm("portal.suggest_event", anonymous)
         
         for forum_id in forum_id_list:
             forum = Forum.objects.get(id=forum_id)
