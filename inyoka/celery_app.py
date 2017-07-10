e416c364a2dde85fa5c0a4b235ef4db23fb83db5
Stefan J. Betz <info@stefan-betz.net>
2017-07-10 20:39:22 +0200

    Updated Celery to 4.0.2

--- inyoka/celery_app.py
+++ inyoka/celery_app.py
@@ -32,7 +32,7 @@ app = Celery('inyoka')
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
-app.config_from_object('django.conf:settings')
+app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Autodiscover does not work with django app objects at the moment (celery 3.1)
 # app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
