df1baf5ed6b18d47d00ade419116ba5ca68dc783
Lyra <lyra2108@gmail.com>
2017-08-26 11:58:05 +0200

    Split the subscription test into smaller tests
    If a test fails it is simpler to debug it. Because you do not have the
    other test logic which might cause side effects.

--- tests/apps/portal/test_subscription.py
+++ tests/apps/portal/test_subscription.py
@@ -5,15 +5,13 @@ from guardian.shortcuts import assign_perm, remove_perm
 
 from inyoka.forum.models import (
     Forum,
-    Topic,
-)
+    Topic)
 from inyoka.portal.models import Subscription
 from inyoka.portal.user import User
 from inyoka.utils.test import AntiSpamTestCaseMixin, InyokaClient, TestCase
 
 
 class TestSubscription(AntiSpamTestCaseMixin, TestCase):
-
     client_class = InyokaClient
 
     def setUp(self):
@@ -23,38 +21,44 @@ class TestSubscription(AntiSpamTestCaseMixin, TestCase):
         self.user = User.objects.register_user('user', 'user@example.com', 'user', False)
         self.forum = Forum.objects.create(name='Forum')
         self.topic = Topic.objects.create(title='A test Topic', author=self.user,
-                forum=self.forum)
+                                          forum=self.forum)
 
         self.registered = Group.objects.get(name=settings.INYOKA_REGISTERED_GROUP_NAME)
-
-
-
-
-    def test_subscribe(self):
         self.client.login(username='user', password='user')
-        registered = Group.objects.get(name=settings.INYOKA_REGISTERED_GROUP_NAME)
-        assign_perm('forum.view_forum', registered, self.forum)
 
-        self.client.get('/topic/%s/subscribe/' % self.topic.slug)
-        self.assertTrue(
-            Subscription.objects.user_subscribed(self.user, self.topic))
+    def test_should_subscribe_to_topic_if_user_has_permission(self):
+        self.set_up_subscription_to_topic()
 
-        # Test for unsubscribe-link in the usercp if the user has no more read
-        # access to a subscription
-        remove_perm('forum.view_forum', registered, self.forum)
+        self.assertTrue(Subscription.objects.user_subscribed(self.user, self.topic),
+                        "The user should be a subscriber for this topic")
+
+    def test_should_contain_unsubscribe_link_in_subscription_list_if_user_has_no_view_permission(self):
+        self.set_up_subscription_to_topic()
+        remove_perm('forum.view_forum', self.registered, self.forum)
         cache.clear()
-        response = self.client.get('/usercp/subscriptions/', {}, False,
-                                   HTTP_HOST=settings.BASE_DOMAIN_NAME)
+
+        response = self.client.get('/usercp/subscriptions/', {}, False, HTTP_HOST=settings.BASE_DOMAIN_NAME)
+
         self.assertTrue(
             ('/topic/%s/unsubscribe/?next=' % self.topic.slug)
             in response.content.decode("utf-8")
         )
 
+    def test_should_forward_to_defined_url_after_unsubcribe(self):
+        self.set_up_subscription_to_topic()
         forward_url = 'http://%s/myfwd' % settings.BASE_DOMAIN_NAME
-        response = self.client.get(
-            '/topic/%s/unsubscribe/' % self.topic.slug,
-            {'next': forward_url}
-        )
-        self.assertFalse(
-            Subscription.objects.user_subscribed(self.user, self.topic))
+
+        response = self.client.get('/topic/%s/unsubscribe/' % self.topic.slug, {'next': forward_url})
+
         self.assertEqual(response['location'], forward_url)
+
+    def test_should_unsubcribe_from_topic(self):
+        self.set_up_subscription_to_topic()
+
+        self.client.get('/topic/%s/unsubscribe/' % self.topic.slug)
+
+        self.assertFalse(Subscription.objects.user_subscribed(self.user, self.topic))
+
+    def set_up_subscription_to_topic(self):
+        assign_perm('forum.view_forum', self.registered, self.forum)
+        self.client.get('/topic/%s/subscribe/' % self.topic.slug)
