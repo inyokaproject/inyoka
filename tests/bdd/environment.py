debc8b483180ace0857f6748f6bfb1dbf09043e2
Lyra <lyra2108@gmail.com>
2017-11-26 00:51:34 +0100

    Save screen shots of failed steps to make debugging easier

--- tests/bdd/environment.py
+++ tests/bdd/environment.py
@@ -9,6 +9,7 @@ os.environ['DJANGO_LIVE_TEST_SERVER_ADDRESS'] = settings.BASE_DOMAIN_NAME
 
 def before_all(context):
     context.SERVER_URL = "http://" + settings.BASE_DOMAIN_NAME
+    context.LOG_DIR = "bdd_screenshots"
 
     if settings.HEADLESS:
         context.browser = webdriver.PhantomJS('node_modules/phantomjs-prebuilt/bin/phantomjs')
@@ -24,7 +25,16 @@ def before_scenario(context, scenario):
     context.browser.implicitly_wait(10)
 
 
-def after_scenario(context, feature):
+def after_step(context, step):
+    if step.status == "failed":
+        directory = os.path.join(context.LOG_DIR, context.feature.name, context.scenario.name)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        filename = os.path.join(directory, step.name + ".png")
+        context.browser.get_screenshot_as_file(filename)
+
+
+def after_scenario(context, scenario):
     if not settings.HEADLESS:
         context.browser.quit()
 
