0f9c835b0748e7fb8d4278f9da2f1691653404e9
Stefan J. Betz <info@stefan-betz.net>
2018-01-20 14:54:45 +0100

    Updated clean_database.sql for PostgreSQL
    * Removed no longer existing tables
    * Foreign Key Checks are no longer an issue

--- docs/clean_database.sql
+++ docs/clean_database.sql
@@ -1,26 +1,7 @@
-SET FOREIGN_KEY_CHECKS = 0;
-# Remove unused tabels
-DROP TABLE IF EXISTS celery_taskmeta;
-DROP TABLE IF EXISTS celery_tasksetmeta;
-DROP TABLE IF EXISTS django_openid_association;
-DROP TABLE IF EXISTS django_openid_nonce;
-DROP TABLE IF EXISTS djcelery_crontabschedule;
-DROP TABLE IF EXISTS djcelery_intervalschedule;
-DROP TABLE IF EXISTS djcelery_periodictask;
-DROP TABLE IF EXISTS djcelery_periodictasks;
-DROP TABLE IF EXISTS djcelery_taskstate;
-DROP TABLE IF EXISTS djcelery_workerstate;
-DROP TABLE IF EXISTS djkombu_message;
-DROP TABLE IF EXISTS djkombu_queue;
-DROP TABLE IF EXISTS south_migrationhistory;
+-- Remove content from user sensetive tabels
+TRUNCATE portal_privatemessage, portal_privatemessageentry, django_session;
 
-# Remove content from user sensetive tabels
-DELETE FROM portal_privatemessage;
-DELETE FROM portal_privatemessageentry;
-DELETE FROM django_session;
-
-# Remove personal user informations
+-- Remove personal user informations
 UPDATE portal_user SET password = 'invalid';
 UPDATE portal_user SET email = CONCAT(username, '@ubuntuusers.invalid');
 UPDATE portal_user SET jabber = '', signature = '', location = '', gpgkey = '', website = '', launchpad = '';
-SET FOREIGN_KEY_CHECKS = 1;
