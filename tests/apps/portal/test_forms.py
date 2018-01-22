67bc6cf2eb196514e5780c715acb21e9336f72e1
Christoph Volkert <inyoka@christoph-volkert.de>
2018-01-22 23:54:48 +0100

    Add basic tests for portal.forms.EditFileForm

--- tests/apps/portal/test_forms.py
+++ tests/apps/portal/test_forms.py
@@ -10,10 +10,14 @@
 """
 from functools import partial
 
-from inyoka.portal.models import StaticPage
+from os import path
+
+from django.core.files.uploadedfile import SimpleUploadedFile
+
+from inyoka.portal.models import StaticPage, StaticFile
 from inyoka.utils.test import TestCase
 
-from inyoka.portal.forms import EditStaticPageForm
+from inyoka.portal.forms import EditFileForm, EditStaticPageForm
 
 
 class TestEditStaticPageForm(TestCase):
@@ -111,3 +115,63 @@ class TestEditStaticPageForm(TestCase):
         form = self.form_create({'key': '', 'title': ' ', 'content': 'foo'})
         self.assertTrue(form.is_valid())
         self.assertEqual(form.cleaned_data['key'], '-')
+
+
+class TestEditFileForm(TestCase):
+    path_file1 = path.join(path.dirname(__file__), 'test_attachment.png')
+    path_file2 = path.join(path.dirname(__file__), 'test_attachment2.png')
+
+    def test_update(self):
+        with open(self.path_file1, 'rb') as f:
+            upload_object = SimpleUploadedFile(f.name, f.read())
+            file = StaticFile.objects.create(identifier='test_attachment.png', file=upload_object)
+
+        with open(self.path_file2, 'rb') as picture_new:
+            upload_object = SimpleUploadedFile(picture_new.name, picture_new.read())
+            form = EditFileForm(instance=file, files={'file': upload_object})
+            self.assertTrue(form.is_valid())
+
+        # identifier is still from the first image
+        self.assertEquals(StaticFile.objects.all()[0].identifier, 'test_attachment.png')
+
+    def test_create_file(self):
+        with open(self.path_file1, 'rb') as picture_for_upload:
+            upload_object = SimpleUploadedFile(picture_for_upload.name, picture_for_upload.read())
+            form = EditFileForm(files={'file': upload_object})
+
+            self.assertTrue(form.is_valid())
+
+    def test_create_with_save(self):
+        with open(self.path_file1, 'rb') as picture_for_upload:
+            upload_object = SimpleUploadedFile(picture_for_upload.name, picture_for_upload.read())
+            form = EditFileForm(files={'file': upload_object})
+            created_object = form.save()
+
+        self.assertEquals(created_object.identifier, 'test_attachment.png')
+        self.assertEquals(StaticFile.objects.count(), 1)
+
+    def test_create_save_commit_false(self):
+        with open(self.path_file1, 'rb') as picture_for_upload:
+            upload_object = SimpleUploadedFile(picture_for_upload.name, picture_for_upload.read())
+            form = EditFileForm(files={'file': upload_object})
+            form.save(commit=False)
+
+        self.assertEquals(StaticFile.objects.count(), 0)
+
+    def test_create_with_duplicate(self):
+        with open(self.path_file1, 'rb') as f:
+            upload_object = SimpleUploadedFile(f.name, f.read())
+            StaticFile.objects.create(identifier='test_attachment.png', file=upload_object)
+
+            form = EditFileForm(files={'file': upload_object})
+            self.assertFalse(form.is_valid())
+            self.assertIn(u'Another file with this name already exists. Please edit this file.', form.errors['file'])
+
+    def test_create_with_duplicate_case_insensitive(self):
+        with open(self.path_file1, 'rb') as f:
+            upload_object = SimpleUploadedFile(f.name, f.read())
+            StaticFile.objects.create(identifier='TEST_attachment.png', file=upload_object)
+
+            form = EditFileForm(files={'file': upload_object})
+            self.assertFalse(form.is_valid())
+            self.assertIn(u'Another file with this name already exists. Please edit this file.', form.errors['file'])
