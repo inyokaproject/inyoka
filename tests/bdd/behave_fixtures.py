ba48576f0027ec06405350dda34a0402588370e6
Lyra <lyra2108@gmail.com>
2018-05-12 02:02:40 +0200

    Restore the BDD test to the state were the DB is wiped before the tests

--- tests/bdd/behave_fixtures.py
+++ tests/bdd/behave_fixtures.py
@@ -1,18 +1,16 @@
-from behave import fixture
 import django
-
+from behave import fixture
 from django.conf import settings
-from django.core.cache import cache
 from django.test.runner import DiscoverRunner
 from django.test.testcases import LiveServerTestCase
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.options import Options
 
 
 @fixture
 def django_test_runner(context):
     django.setup()
-    context.test_runner = DiscoverRunner(keepdb=False)
+    context.test_runner = DiscoverRunner()
     context.old_db_config = context.test_runner.setup_databases()
     context.test_runner.setup_test_environment()
     yield
@@ -24,16 +22,15 @@ def django_test_runner(context):
 def django_test_case(context):
     context.test_case = LiveServerTestCase
     context.test_case.host = 'ubuntuusers.local'
+    context.test_case = context.test_case(methodName='run')
+    context.test_case._pre_setup()
     context.test_case.setUpClass()
+    context.test_case()
     context.base_url = context.test_case.live_server_url
     yield context.base_url
     context.test_case.tearDownClass()
+    context.test_case._post_teardown()
     del context.test_case
-
-
-@fixture()
-def setup_cache(context):
-    cache.clear()
 
 
 @fixture
--- tests/bdd/environment.py
+++ tests/bdd/environment.py
@@ -3,7 +3,7 @@ import os
 from behave import use_fixture
 from django.conf import settings
 
-from tests.bdd.behave_fixtures import browser_chrome, django_test_case, django_test_runner, setup_cache
+from tests.bdd.behave_fixtures import browser_chrome, django_test_case, django_test_runner
 from tests.bdd.steps.utils import take_screenshot
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.bdd.settings.headless'
@@ -11,14 +11,17 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.bdd.settings.headless'
 
 def before_all(context):
     context.LOG_DIR = "bdd_screenshots"
+    use_fixture(django_test_runner, context)
+    use_fixture(browser_chrome, context)
 
 
 def before_scenario(context, scenario):
-    use_fixture(django_test_runner, context)
-    use_fixture(django_test_case, context)
-    use_fixture(setup_cache, context)
-    use_fixture(browser_chrome, context)
+    from django.contrib.auth.models import Group
 
+    use_fixture(django_test_case, context)
+
+    Group.objects.get_or_create(name=settings.INYOKA_REGISTERED_GROUP_NAME)
+    Group.objects.get_or_create(name=settings.INYOKA_ANONYMOUS_GROUP_NAME)
     settings.BASE_DOMAIN_NAME = context.base_url[7:]
     settings.MEDIA_URL = '//media.%s/' % settings.BASE_DOMAIN_NAME
     settings.STATIC_URL = '//static.%s/' % settings.BASE_DOMAIN_NAME
