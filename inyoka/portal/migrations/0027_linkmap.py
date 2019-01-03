5f487ce1a378a4a975824e48bdc84e6b76f17767
Christoph Volkert <inyoka@christoph-volkert.de>
2019-01-03 03:05:06 +0100

    migration

--- inyoka/portal/migrations/0027_linkmap.py
+++ inyoka/portal/migrations/0027_linkmap.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import migrations, models
 import django.core.validators
+import inyoka.portal.models
 
 
 class Migration(migrations.Migration):
@@ -16,9 +17,9 @@ class Migration(migrations.Migration):
             name='Linkmap',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('token', models.CharField(unique=True, max_length=128, verbose_name='Token', validators=[django.core.validators.RegexValidator(regex=b'[a-z\\-_]+', message='Only lowercase letters, - and _ allowed.')])),
-                ('url', models.URLField(verbose_name='Link')),
-                ('icon', models.ImageField(upload_to='linkmap/icons', verbose_name='Icon', blank=True)),
+                ('token', models.CharField(unique=True, max_length=128, verbose_name='Token', validators=[django.core.validators.RegexValidator(regex=b'^[a-z\\-_]+[1-9]*$', message='Only lowercase letters, - and _ allowed. Numbers as postfix.')])),
+                ('url', models.CharField(max_length=200, verbose_name='Link', validators=[inyoka.portal.models.url_scheme_validator])),
+                ('icon', models.ImageField(upload_to=b'linkmap/icons', verbose_name='Icon', blank=True)),
             ],
         ),
     ]
