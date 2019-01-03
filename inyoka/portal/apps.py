e6abb7c969a42146f9269c17b6348d97aff2155c
Christoph Volkert <inyoka@christoph-volkert.de>
2019-01-03 02:01:14 +0100

    Generate css for linkmap once at startup

--- inyoka/portal/apps.py
+++ inyoka/portal/apps.py
@@ -5,3 +5,16 @@ from django.apps import AppConfig
 class PortalAppConfig(AppConfig):
     name = 'inyoka.portal'
     verbose_name = 'Portal'
+
+    css_generated = False
+
+    def ready(self):
+        """
+        Will be executed once on startup
+
+        see https://docs.djangoproject.com/en/1.8/ref/applications/#django.apps.AppConfig.ready
+        """
+        if not self.css_generated:
+            from inyoka.portal.models import Linkmap
+            Linkmap.objects.generate_css()
+            self.css_generated = True
