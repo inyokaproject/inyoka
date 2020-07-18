5cb1be775e890e661171c872ef505483d2256065
Christoph Volkert <inyoka@christoph-volkert.de>
2020-07-19 00:25:58 +0200

    Use seperate redis-dbs for postgres and sqlite
    Both can potentially erase each ones cache when running parallel.

--- tests/settings/postgresql.py
+++ tests/settings/postgresql.py
@@ -11,4 +11,10 @@ DATABASES = {
     }
 }
 
+# use redis databases starting from 10, so they do not infer with sqlite
+CACHES['content']['LOCATION'] = 'redis://{}:6379/10'.format(test_host or 'localhost')
+CACHES['default']['LOCATION'] = 'redis://{}:6379/11'.format(test_host or 'localhost')
+BROKER_URL = 'redis://{}:6379/10'.format(test_host or 'localhost')
+CELERY_RESULT_BACKEND = 'redis://{}:6379/10'.format(test_host or 'localhost')
+
 TEST_OUTPUT_FILE_NAME = 'postgresql.xml'
