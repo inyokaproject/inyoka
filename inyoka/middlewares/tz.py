ffe903d4d9b46ffecce3df54696f4694dd3f4913
Christoph Volkert <inyoka@christoph-volkert.de>
2020-08-23 02:39:47 +0200

    Use new style middleware classes
    As first step only tz uses a completly new style. All other middlewares
    use the migration path via MiddlewareMixin. If latter is not inherited directly,
    it is used indirectly from a django middleware.
    F.e. inyoka.middlewares.SessionMiddleware inherits from
    django.contrib.sessions.middleware.SessionMiddleware, which uses
    MiddlewareMixin.
    
    warning was
    
    /home/chris/.venvs/inyoka3/lib/python3.8/site-packages/django/core/handlers/base.py:49: RemovedInDjango20Warning: Old-style middleware using settings.MIDDLEWARE_CLASSES is deprecated. Update your middleware and use settings.MIDDLEWARE instead.
      warnings.warn(
    
    see
    https://docs.djangoproject.com/en/3.1/topics/http/middleware/

--- inyoka/middlewares/tz.py
+++ inyoka/middlewares/tz.py
@@ -11,10 +11,24 @@
 from django.utils import timezone
 
 
-class TimezoneMiddleware(object):
-    def process_request(self, request):
+class TimezoneMiddleware:
+    def __init__(self, get_response):
+        """One-time configuration and initialization."""
+        self.get_response = get_response
+
+    def __call__(self, request):
+        # Code to be executed for each request before
+        # the view (and later middleware) are called.
+
         tz = request.session.get('django_timezone')
         if tz:
             timezone.activate(tz)
         else:
             timezone.deactivate()
+
+        response = self.get_response(request)
+
+        # Code to be executed for each request/response after
+        # the view is called.
+
+        return response
