582c7799368b6ed1ef5ad7dfc7e3e9ac441425c3
Christoph Volkert <inyoka@christoph-volkert.de>
2020-11-07 18:01:15 +0100

    Update comment in inyoka's __init__

--- inyoka/__init__.py
+++ inyoka/__init__.py
@@ -3,40 +3,38 @@
     inyoka
     ~~~~~~
 
-    The inyoka portal system.  The system is devided into multiple modules
-    to which we refer as applications.  The name inyoka means "snake" in
+    The inyoka portal system. The system is devided into multiple modules
+    to which we refer as applications. The name inyoka means "snake" in
     zulu and was chosen because it's a python application *cough*.
 
-    Although the software is based on django we use different idoms and a
-    non standard template engine called Jinja.  The basic application
-    structure is explained below.
+    Although the software is based on django we use (sometimes) different
+    idoms and a non standard template engine Jinja. The basic
+    application structure is explained below.
 
 
     Requirements
     ============
 
-    The code is mostly tested with MySQL but should work with PostgreSQL,
-    SQLite or any other database Django supports.
+    The code is mostly tested with PostgreSQL but should work with SQLite or
+    any other database Django supports.
 
-    Additionally Jinja 2.5 or higher is required.  For the planet application
-    the feedparser library must be installed.  Additionally chardet is
-    recommended so that it can better guess broken encodings of feeds.
-    For the pastebin, wiki and some other parts `pygments` 1.4 or higher must
-    be available. MySQL must support InnoDB or any other transaction engine
-    like falcon (untested though).  For incoming HTML data that is converted
-    to XHTML we also need html5lib.
+    For the planet application the feedparser library must be installed.
+    Additionally chardet is recommended so that it can better guess broken
+    encodings of feeds. For the pastebin, wiki and some other parts `pygments`
+    must be available. For incoming HTML data that is converted to XHTML we
+    also need html5lib.
 
-    We're using the recent stable django releases.
+    We're using the recent LTS django releases.
 
-    For deployment redis is the preferred caching system.  Otherwise use
+    For deployment redis is the preferred caching system. Otherwise use
     many threads and few processes and enable `locmem`.
 
 
     Configuration
     =============
 
-    The default configuration the development, test and production
-    configuration files should start import is in `inyoka.default_settings`.
+    The configuration for development, test and production
+    should start import `inyoka.default_settings`.
     This file is not referenced by the application code itself which means
     that you can import it as part of the django setup without causing
     circular bootstrapping dependencies.
@@ -58,7 +56,7 @@
     `init.sh` file (``source init.sh`` or ``. init.sh``).
 
     Then you can use the Makefile which provides comments for resetting the
-    database, starting the server, building the documentation etc.  For more
+    database, starting the server, building the documentation etc. For more
     details have a look at the Makefile.
 
 
@@ -68,35 +66,35 @@
     The following applications are part of inyoka so far:
 
     `portal`
-        The index application.  On no subdomain and the portal page.
-        Aggregates recent ikhaya posts and similar stuff.  Also shows the
+        The index application. On no subdomain and the portal page.
+        Aggregates recent ikhaya posts and similar stuff. Also shows the
         number of online users.
 
     `forum`
-        The forum component.  It's inspired by phpBB2 which was previously
-        used on the German ubuntuusers.de webpage.  Some of the functionallity
-        was extended over time though.  Especially an improved notification
-        system, attachments and subforums (latter new in inyoka)
+        The forum component. It's inspired by phpBB2 which was previously
+        used on the German ubuntuusers.de webpage. Some of the functionallity
+        was extended over time though. Especially an improved notification
+        system, attachments and subforums.
 
     `wiki`
-        Moin inspired wiki engine.  It's not yet as advanced as moin 1.7 but
+        Moin inspired wiki engine. It's not yet as advanced as moin 1.7 but
         has revisioned pages and a better parser which can savely generate XML
-        based output formats.  The wiki parser also has some
+        based output formats. The wiki parser also has some
         BBCode elements for compatibility with the old phpBB syntax and is
         used in other components (`forum`, `ikhaya`, ...) as well.
 
     `planet`
-        A planet planet like feed aggregator.  It has archives and santized
+        A planet like feed aggregator. It has archives and santized
         input data thanks to feedparser.
 
     `ikhaya`
-        Ikhaya is zulu for `home` and a blog application.  It's used on the
+        Ikhaya is zulu for `home` and a blog application. It's used on the
         German ubuntuusers portal for site wide annoucements and other news.
         It doesn't show up on the planet automatically, for that you have to
         add the ikhaya feed to it like for any other blog too.
 
     `pastebin`
-        A pastebin that uses Pygments for highlighting.  It does not support
+        A pastebin that uses Pygments for highlighting. It does not support
         diffing yet but allows to download pastes.
 
 
