1be090fb26633f44a32f4c35a18b70cb07510564
Christoph Volkert <inyoka@christoph-volkert.de>
2020-12-19 21:53:35 +0100

    Bump version: 0.24.3 → 0.24.4

--- inyoka/__init__.py
+++ inyoka/__init__.py
@@ -118,4 +118,4 @@ import socket
 socket.setdefaulttimeout(10.0)
 
 # Inyoka version is updated through bumpversion and can stay hardcoded here.
-INYOKA_VERSION = "v0.24.3"
+INYOKA_VERSION = "v0.24.4"
