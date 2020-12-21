e9289b6229f1d20e0fa18b5cec120d3b881872fa
Christoph Volkert <inyoka@christoph-volkert.de>
2020-12-21 04:41:51 +0100

    Remove old txt before generating a new one
    Thus, preexisting requirement-files are no more a constrain for pip-tools.

--- inyoka/portal/management/commands/generate_requirements.py
+++ inyoka/portal/management/commands/generate_requirements.py
@@ -32,10 +32,26 @@ class Command(BaseCommand):
                             dest='upgrade_packages', help='Define one or more packages that should be upgraded. '
                                                           'If only the option is given, all packages are upgraded.')
 
-    def generate_requirements_file(self, stage: str, upgrade_all: bool = False, upgrade_packages: List = []) -> None:
+    def _get_requirements_path(self, stage: str) -> str:
         py_major, py_minor, _ = platform.python_version_tuple()
         file = '{}-py{}.{}-{}.txt'.format(sys.platform, py_major, py_minor, stage)
         full_path = os.path.join(self.requirements_path, file)
+
+        return full_path
+
+    def remove_requirements_files(self) -> None:
+        """
+        If nothing should be upgraded, remove the requirement-files of the current environment.
+        Thus, preexisting requirement-files are no more a constrain for pip-tools.
+        """
+        for s in self.stages:
+            try:
+                os.remove(self._get_requirements_path(s))
+            except FileNotFoundError:
+                continue
+
+    def generate_requirements_file(self, stage: str, upgrade_all: bool = False, upgrade_packages: List = []) -> None:
+        full_path = self._get_requirements_path(stage)
 
         program_name = 'pip-compile'
         arguments = ['--allow-unsafe', '--generate-hashes', '--output-file', full_path]
@@ -48,12 +64,21 @@ class Command(BaseCommand):
             arguments += ['--upgrade-package', p]
 
         if stage == self.stage_dev:
-            arguments += [os.path.join(self.requirements_path, 'development.in')]
+            dev_template_file = os.path.join(self.requirements_path, 'development.in')
+            arguments += [dev_template_file]
+
+            # use previously generated production file (for this specific environment)
+            # as constraint in `dev_template_file`
+            with open(dev_template_file, 'r+') as f:
+                lines = f.readlines()
+                lines[0] = '-c ' + os.path.basename(self._get_requirements_path(self.stage_prod)) + '\n'
+                f.seek(0)
+                f.writelines(lines)
 
         custom_env = os.environ
         custom_env["CUSTOM_COMPILE_COMMAND"] = "python manage.py generate_requirements"
 
-        print('Generating', file)
+        print('Generating', full_path)
         try:
             subprocess.run([program_name] + arguments, capture_output=True, check=True, env=custom_env)
         except subprocess.CalledProcessError as e:
@@ -71,6 +96,9 @@ class Command(BaseCommand):
                 upgrade_all = True
             else:
                 upgrade_packages = options.upgrade_packages
+
+        if not upgrade_all and not upgrade_packages:
+            self.remove_requirements_files()
 
         for s in self.stages:
             self.generate_requirements_file(s, upgrade_all, upgrade_packages)
