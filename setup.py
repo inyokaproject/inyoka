312ae64ac6321ab05c4d179607520ad2e4328b81
Christoph Volkert <inyoka@christoph-volkert.de>
2021-01-06 01:21:35 +0100

    setup.py: Explicitly require celery[redis]

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
 
         'Babel',
         'beautifulsoup4==4.6.0',
-        'celery==4.3.0',
+        'celery[redis]==4.3.0',
         'certifi',
         'defusedxml==0.6.0',
         'django-filter==1.0.4',
