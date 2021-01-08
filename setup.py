998deceb67e0ff71d6e7e90b3800e4fdbf741831
Christoph Volkert <inyoka@christoph-volkert.de>
2021-01-08 04:50:18 +0100

    Django 2.1

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     packages=find_packages(include=('inyoka*',)),
     python_requires='>=3.5, <4',
     install_requires=[
-        'Django<2.1',
+        'Django<2.2',
         'pip-tools',
 
         'Babel',
