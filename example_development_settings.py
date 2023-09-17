329ed8a364277b821cb4ab04f425c064199d17db
Christoph Volkert <inyoka@christoph-volkert.de>
2023-09-17 15:13:42 +0200

    Use simplified DB backend name for postgresql

--- example_development_settings.py
+++ example_development_settings.py
@@ -3,7 +3,7 @@ from inyoka.default_settings import *
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'ubuntuusers',
         'USER': 'root',
         'PASSWORD': '',
