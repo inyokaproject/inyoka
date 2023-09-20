5e7c690a7a759ef8cab5a80a41bb17ff81754396
Christoph Volkert <inyoka@christoph-volkert.de>
2023-09-20 23:41:01 +0200

    Pin dependencies for django 3.2

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     packages=find_packages(include=('inyoka*',)),
     python_requires='>=3.9, <4',
     install_requires=[
-        'Django<3.2',
+        'Django<4.0',
         'pip-tools',
 
         'Babel',
