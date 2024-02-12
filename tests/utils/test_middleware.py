356241cca28ad7e5854872dbcaab37bde379358d
Christoph Volkert <inyoka@christoph-volkert.de>
2024-02-12 22:52:04 +0100

    CommonServicesMiddleware: Filter control characters in all POST fields
    Filtering in the middleware has the benefit that it affects every form.
    
    Use python translate table and not a regex.
    Some naive benchmarking to compare:
    
    >>> import timeit
    >>> import re
    >>> text='abc'*2000
    >>> timeit.timeit(lambda: re.sub(r'[\x00-\x08\x0B-\x0C\x0E-\x1F]', '', text), number=1000)
    0.03747229900000093
    
    ------------
    
    >>> table = {0x0B: None, 0x0C: None}
    >>> for i in range(0x0, 0x08+1):
    ...     table[i] = None
    ...
    >>> for i in range(0x0E, 0x1F+1):
    ...     table[i] = None
    ...
    >>> import timeit
    >>> text='abc'*2000
    >>> timeit.timeit(lambda: text.translate(table), number=1000)
    0.010054252999907476
    
    ------------
    
    to_remove =  set('\x00\x01\x02\x03\x04\x05\x06\x07\x08\x0b\x0c\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f')
    
    import timeit
    text = 'abc'*2000
    def remove_control_chars():
        new_text = ''
        for s in text:
            if s not in to_remove:
                new_text += s
    
    >>> timeit.timeit(remove_control_chars, number=1000)
    0.3113498080001591
    
    ------------
    
    >>> import timeit
    >>> text='abc'*2000
    >>> timeit.timeit(lambda: text.replace('\x00', '').replace('\x01', '').replace('\x02', '').replace('\x03', '').replace('\x04', '').replace('\x05', '').replace('\x06', '').replace('\x07', '').replace('\x08', '').replace('\x0b', '').replace('\x0c', '').replace('\x0e', '').replace('\x0f', '').replace('\x10', '').replace('\x11', '').replace('\x12', '').replace('\x13', '').replace('\x14', '').replace('\x15', '').replace('\x16', '').replace('\x17', '').replace('\x18', '').replace('\x19', '').replace('\x1a', '').replace('\x1b', '').replace('\x1c', '').replace('\x1d', '').replace('\x1e', '').replace('\x1f', ''), number=1000)
    0.09860884699992312

--- tests/utils/test_middleware.py
+++ tests/utils/test_middleware.py
@@ -8,7 +8,9 @@
     :license: BSD, see LICENSE for more details.
 """
 from django.conf import settings
+from django.test import RequestFactory
 
+from inyoka.middlewares.common import CommonServicesMiddleware
 from inyoka.utils.test import InyokaClient, TestCase
 
 
@@ -38,3 +40,10 @@ class TestServiceMiddleware(TestCase):
         url = f'http://{ settings.BASE_DOMAIN_NAME }/?__service__=portal.toggle_sidebar%27%7C%7CDBMS_PIPE.RECEIVE'
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
+
+    def test_control_characters_stripped_in_post(self):
+        self.factory = RequestFactory()
+        data = {'foobar': 'control characters \x08 \x0f in text'}
+        request = self.factory.post('/', data)
+        CommonServicesMiddleware(lambda x: x).process_request(request)
+        self.assertEqual(request.POST['foobar'], 'control characters   in text')
