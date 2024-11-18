0fb35124fb31cf23ee5f58b40e721a98ecab5f48
Christoph Volkert <inyoka@christoph-volkert.de>
2024-11-18 21:21:22 +0100

    Fix typo

--- inyoka/portal/views.py
+++ inyoka/portal/views.py
@@ -1178,7 +1178,7 @@ def grouplist(request, page=1):
 @login_required
 @templated('portal/group.html')
 def group(request, name, page=1):
-    """Shows the informations about the group named `name`."""
+    """Shows the information about the group named `name`."""
     if name == settings.INYOKA_REGISTERED_GROUP_NAME and not request.user.has_perm('portal.change_user'):
         raise Http404
     group = Group.objects.get(name__iexact=name)
