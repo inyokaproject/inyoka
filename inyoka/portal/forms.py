5b386f5791a702d1a8f67086cac155384d1af173
Christoph Volkert <inyoka@christoph-volkert.de>
2024-12-23 00:49:50 +0100

    EditUserStatusForm: Remove not need lines

--- inyoka/portal/forms.py
+++ inyoka/portal/forms.py
@@ -539,9 +539,6 @@ class CreateUserForm(forms.Form):
 
 
 class EditUserStatusForm(forms.ModelForm):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['banned_until'].localize = True
 
     class Meta:
         model = User
