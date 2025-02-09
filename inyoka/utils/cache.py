deccdf21ef48749c187bd883de0f158e10d0a5f8
Christoph Volkert <inyoka@christoph-volkert.de>
2025-02-09 23:42:56 +0100

    utils.cache: Remove outdated docstring
    Fix a sphinx warning that the class `RequestCache` can not be found, as it
    does not exist anymore.

--- inyoka/utils/cache.py
+++ inyoka/utils/cache.py
@@ -2,12 +2,7 @@
     inyoka.utils.cache
     ~~~~~~~~~~~~~~~~~~
 
-    The caching infrastructure of Inyoka.
-
-    On top of the django cache client that speaks directly to either redis
-    or caches in-memory we have a :class:`RequestCache` that caches
-    redis-commands in a thread-local dictionary.  This saves a lot of
-    redis-commands in some scenarios.
+    Custom caching classes for Inyoka.
 
     :copyright: (c) 2007-2025 by the Inyoka Team, see AUTHORS for more details.
     :license: BSD, see LICENSE for more details.
