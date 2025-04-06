e2f7d5bca9f2115390efce199cbb4bb40f6ba600
Christoph Volkert <inyoka@christoph-volkert.de>
2025-04-07 00:46:10 +0200

    portal.tasks: Move inline queryset to own variable

--- inyoka/portal/tasks.py
+++ inyoka/portal/tasks.py
@@ -44,12 +44,10 @@ def _clean_expired_users():
     deleted after ACTIVATION_HOURS (default 48h).
     """
     expired_datetime = dj_timezone.now() - timedelta(hours=settings.ACTIVATION_HOURS)
+    user_query = (User.objects.filter(status=0).filter(date_joined__lte=expired_datetime)
+                     .exclude(username__in={settings.ANONYMOUS_USER_NAME, settings.INYOKA_SYSTEM_USER}))
 
-    for user in (User.objects.filter(status=0)
-                     .filter(date_joined__lte=expired_datetime)
-                     .exclude(username__in={
-                         settings.ANONYMOUS_USER_NAME,
-                         settings.INYOKA_SYSTEM_USER})):
+    for user in user_query:
         if not user.has_content():
             logger.info(f'Deleting expired User {user.username}')
             user.delete()
