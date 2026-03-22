a02a15d287077a276521124f1917c3ccd5513679
Christoph Volkert <inyoka@christoph-volkert.de>
2026-03-22 23:54:44 +0100

    Bump version: 1.52.7 → 1.52.8

--- inyoka/__init__.py
+++ inyoka/__init__.py
@@ -10,4 +10,4 @@
 from .celery_app import app as celery_app  # noqa
 
 # Inyoka version is updated through bumpversion and can stay hardcoded here.
-INYOKA_VERSION = "1.52.7"
+INYOKA_VERSION = "1.52.8"
