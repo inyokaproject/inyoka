9b10123e5c28fbf8b9c8f3a4a9ed1cda8ec0635f
Christoph Volkert <inyoka@christoph-volkert.de>
2026-05-22 15:40:22 +0200

    ikhaya, macros: Reduce scope for try except to ORMs `get`
    The file variable is later always in scope
    (or the method returns on failure)

--- inyoka/ikhaya/macros.py
+++ inyoka/ikhaya/macros.py
@@ -25,27 +25,32 @@ def build_ikhaya_picture_node(sender, context, format, **kwargs):
         return None
 
     target, width, height = (sender.target, sender.width, sender.height)
+
     try:
         file = StaticFile.objects.get(identifier=target)
-        if (width or height) and os.path.exists(file.file.path):
-            tt = target.rsplit('.', 1)
-            dimension = '%sx%s' % (width and int(width) or '',
-                                   height and int(height) or '')
-            target = '%s%s.%s' % (tt[0], dimension, tt[1])
-
-            destination = os.path.join(settings.MEDIA_ROOT, 'portal/thumbnails', target)
-            thumb = get_thumbnail(file.file.path, destination, width, height)
-            if thumb:
-                source = os.path.join(settings.MEDIA_URL, 'portal/thumbnails', thumb.rsplit('/', 1)[1])
-            else:
-                # fallback to the original file
-                source = os.path.join(settings.MEDIA_URL, file.file.name)
-        else:
-            source = url_for(file)
-        img = nodes.Image(source, sender.alt, class_='image-' +
-                          (sender.align or 'default'), title=sender.title)
-        if (width or height) and file is not None:
-            return nodes.Link(url_for(file), [img])
-        return img
     except StaticFile.DoesNotExist:
         return None
+
+    if (width or height) and os.path.exists(file.file.path):
+        tt = target.rsplit('.', 1)
+        dimension = '%sx%s' % (width and int(width) or '',
+                               height and int(height) or '')
+        target = '%s%s.%s' % (tt[0], dimension, tt[1])
+
+        destination = os.path.join(settings.MEDIA_ROOT, 'portal/thumbnails', target)
+        thumb = get_thumbnail(file.file.path, destination, width, height)
+        if thumb:
+            source = os.path.join(settings.MEDIA_URL, 'portal/thumbnails', thumb.rsplit('/', 1)[1])
+        else:
+            # fallback to the original file
+            source = os.path.join(settings.MEDIA_URL, file.file.name)
+    else:
+        source = url_for(file)
+
+    img = nodes.Image(source, sender.alt, class_='image-' +
+                      (sender.align or 'default'), title=sender.title)
+
+    if (width or height) and file is not None:
+        return nodes.Link(url_for(file), [img])
+
+    return img
